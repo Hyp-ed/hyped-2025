bb75e80befbbd25de01f8f06ed23f4e063b0e033
Kshitij Sharma <kshitij4sharma@gmail.com>
2024-07-24 23:42:09 +0100

    Use with_state and share InfluxDB connection

--- telemetry/server/src/openmct/data/historical.rs
+++ telemetry/server/src/openmct/data/historical.rs
@@ -1,13 +1,15 @@
-use axum::extract::Query;
+use axum::extract::{Query, State};
 use axum::http::StatusCode;
 use axum::{extract::Path, routing::get, Json, Router};
 use axum_macros::debug_handler;
 use chrono::{DateTime, FixedOffset};
 use influxdb2::models::Query as InfluxQuery;
-use influxdb2::{Client, FromDataPoint};
+use influxdb2::FromDataPoint;
 use serde::Serialize;
 
-pub fn get_routes() -> Router {
+use crate::TelemetryServerState;
+
+pub fn get_routes() -> Router<TelemetryServerState> {
     Router::new().route(
         "/pods/:pod/measurements/:measurement_key",
         get(get_historical_reading),
@@ -16,7 +18,7 @@ pub fn get_routes() -> Router {
 
 #[derive(Debug, FromDataPoint, Serialize)]
 pub struct InfluxHistoricalReading {
-    measurementKey: String,
+    measurement_key: String,
     _time: DateTime<FixedOffset>,
     value: f64,
 }
@@ -32,7 +34,7 @@ pub struct HistoricalReading {
 impl Default for InfluxHistoricalReading {
     fn default() -> Self {
         Self {
-            measurementKey: "".to_string(),
+            measurement_key: "".to_string(),
             _time: DateTime::parse_from_rfc3339("1970-01-01T00:00:00Z").unwrap(),
             value: 0.0,
         }
@@ -50,12 +52,9 @@ struct TimespanQuery {
 async fn get_historical_reading(
     Path((pod, measurement_key)): Path<(String, String)>,
     Query(query): Query<TimespanQuery>,
+    State(state): State<TelemetryServerState>,
 ) -> Result<Json<Vec<HistoricalReading>>, (StatusCode, String)> {
-    let host = std::env::var("INFLUXDB_HOST").unwrap();
-    let org = std::env::var("INFLUXDB_ORG").unwrap();
-    let token = std::env::var("INFLUXDB_TOKEN").unwrap();
     let telemetry_bucket = std::env::var("INFLUXDB_TELEMETRY_BUCKET").unwrap();
-    let client = Client::new(host, org, token);
 
     let qs = format!(
         "from(bucket: \"{}\")
@@ -74,14 +73,17 @@ async fn get_historical_reading(
     );
 
     let query = InfluxQuery::new(qs.to_string());
-    let influx_res = client.query::<InfluxHistoricalReading>(Some(query)).await;
+    let influx_res = state
+        .influxdb_client
+        .query::<InfluxHistoricalReading>(Some(query))
+        .await;
 
     match influx_res {
         Ok(res) => {
             let res = Json(
                 res.into_iter()
                     .map(|reading| HistoricalReading {
-                        id: reading.measurementKey,
+                        id: reading.measurement_key,
                         timestamp: reading._time.timestamp_millis(),
                         value: reading.value,
                     })
