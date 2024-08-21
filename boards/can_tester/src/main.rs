91cf7c618be0022cfa0962937ee1a03836a4ee94
Kshitij Sharma <kshitij4sharma@gmail.com>
2024-08-21 12:30:42 +0100

    Initialise Can Board

---
+++ boards/can_tester/src/main.rs
@@ -0,0 +1,82 @@
+#![no_std]
+#![no_main]
+
+use core::num::{NonZeroU16, NonZeroU8};
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::can::{
+    filter::Mask32, frame::Frame, Can, CanTx, Fifo, Rx0InterruptHandler, Rx1InterruptHandler,
+    SceInterruptHandler, StandardId, TxInterruptHandler,
+};
+use embassy_stm32::gpio::{Input, Pull};
+use embassy_stm32::peripherals::CAN3;
+use embassy_stm32::{bind_interrupts, can};
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
+
+bind_interrupts!(struct Irqs {
+    CAN3_RX0 => Rx0InterruptHandler<CAN3>;
+    CAN3_RX1 => Rx1InterruptHandler<CAN3>;
+    CAN3_SCE => SceInterruptHandler<CAN3>;
+    CAN3_TX => TxInterruptHandler<CAN3>;
+});
+
+#[embassy_executor::task]
+pub async fn send_can_message(tx: &'static mut CanTx<'static>) {
+    let mut counter = 0;
+    loop {
+        let frame = Frame::new_data(
+            unwrap!(StandardId::new(2)),
+            &[0xde, 0xad, 0xbe, 0xef, counter as u8],
+        )
+        .unwrap();
+        tx.write(&frame).await;
+        embassy_time::Timer::after_secs(1).await;
+        counter += 1;
+        info!("Sent {} frames", counter);
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    info!("Hello World!");
+
+    let mut p = embassy_stm32::init(Default::default());
+
+    // Pull up resistor for the RX pin to avoid the 120 Ohm termination resistor
+    let rx_pin = Input::new(&mut p.PA15, Pull::Up);
+    core::mem::forget(rx_pin);
+
+    static CAN: StaticCell<Can<'static>> = StaticCell::new();
+
+    // CAN3_RX and CAN3_TX taken from https://danieleff.github.io/STM32GENERIC/board_Nucleo_F767ZI/
+    let can = CAN.init(Can::new(p.CAN3, p.PA8, p.PA15, Irqs));
+    can.modify_filters()
+        .enable_bank(0, Fifo::Fifo0, Mask32::accept_all());
+
+    // Use default for now until we figure out what we need
+    // http://www.bittiming.can-wiki.info/
+    can.modify_config()
+        .set_bit_timing(can::util::NominalBitTiming {
+            prescaler: NonZeroU16::new(2).unwrap(),
+            seg1: NonZeroU8::new(13).unwrap(),
+            seg2: NonZeroU8::new(2).unwrap(),
+            sync_jump_width: NonZeroU8::new(1).unwrap(),
+        })
+        .set_loopback(true);
+
+    can.enable().await;
+
+    let (tx, mut rx) = can.split();
+
+    static CAN_TX: StaticCell<CanTx<'static>> = StaticCell::new();
+    let tx = CAN_TX.init(tx);
+    spawner.spawn(send_can_message(tx)).unwrap();
+
+    loop {
+        let envelope = rx.read().await.unwrap();
+        let counter_value = envelope.frame.data()[4];
+        info!("Received frame with counter value: {}", counter_value);
+    }
+}
