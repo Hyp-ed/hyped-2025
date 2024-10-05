eaced103595addc9ecb6593fb25bea502765a185
platinumxy <83834735+platinumxy@users.noreply.github.com>
2024-10-06 00:18:21 +0100

    Halfway through changing python script to rust

---
+++ FirmwareUpdater/src/main.rs
@@ -0,0 +1,99 @@
+use std::{
+    env, fs,
+    io::{self, Write},
+    path::{self, PathBuf},
+    process::exit,
+};
+
+fn inf<'a>(s: &'a str) -> String {
+    format!("\x1b[34;49m[I] {s} \x1b[0m")
+}
+fn que<'a>(s: &'a str) -> String {
+    format!("\x1b[35;49m[Q] {s} \x1b[0m")
+}
+fn err<'a>(s: &'a str) -> String {
+    format!("\x1b[31;49m[E] {s} \x1b[0m")
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let (cargo_pth, trg_toml, trg_conf) = get_paths(&args);
+    println!( // evil print statment that works :)
+        "{}",
+        inf(&format!(
+            "Got location of target toml as {} {}",
+            trg_toml.to_str().unwrap(),
+            format!(
+                "{}",
+                if let Some(s) = trg_conf {
+                    format!("and descovered config file in {}", s.to_str().unwrap())
+                } else {
+                    Default::default()
+                }
+            )
+        ))
+    );
+
+    create_build_path(&cargo_pth);
+}
+
+fn get_input_line(s: String) -> String {
+    print!("{}", s);
+    let _ = io::stdout().flush();
+    let mut buf: String = Default::default();
+    let _ = io::stdin().read_line(&mut buf);
+    while buf.ends_with("\n") || buf.ends_with("\r") {
+        buf.pop();
+    }
+    buf
+}
+
+#[inline] // I know this only affects external packages, however it makes it clear it doenst need to be seprate flow of logic
+fn get_paths(args: &Vec<String>) -> (PathBuf, PathBuf, Option<PathBuf>) {
+    let cargo_pth = path::absolute(if args.len() > 1 { // first arg is bin path
+        args[1].to_owned()
+    } else {
+        get_input_line(que(
+            "Please enter the path to the folder that contains the target Cargo.toml: ",
+        ))
+        .to_owned()
+    });
+
+    if let Err(e) = cargo_pth {
+        println!("{}", err(&format!("Unable to resolve path {e}")));
+        exit(1)
+    };
+    let cargo_pth = cargo_pth.unwrap();
+
+    let trg_toml = cargo_pth.join("Cargo.toml");
+
+    let car_p_meta = fs::metadata(&trg_toml); // if this is valid then cargo_pth must be
+    if car_p_meta.is_err() {
+        println!(
+            "{}",
+            err(&format!(
+                "target Cargo.toml file or its parent dir doesn't exist at {}",
+                trg_toml.to_str().unwrap()
+            ))
+        );
+        exit(2)
+    };
+    let car_p_meta = car_p_meta.unwrap();
+
+    if !car_p_meta.is_file() {
+        println!("{}", err("Target file or dir is invalid format"));
+        exit(2)
+    }
+
+    let trg_conf: Option<PathBuf> =
+        if fs::exists(cargo_pth.join(".cargo\\config.toml")).unwrap_or(false) {
+            Some(cargo_pth.join(".cargo\\config.toml"))
+        } else {
+            None
+        };
+
+    return (cargo_pth, trg_toml, trg_conf);
+}
+
+fn create_build_path(cargo_pth: &PathBuf) {}
