82ad8322fb0807ffcca1f9a561fa9aeca4fa9aa3
Michael Belyaev <michael7belyaev@gmail.com>
2024-10-10 17:13:47 +0100

    implemented preprocess_optical

--- lib/localisation/preprocessing/src/preprocess_optical.rs
+++ lib/localisation/preprocessing/src/preprocess_optical.rs
@@ -1,12 +1,13 @@
 
-//TODOLater: Implement properly!
 fn main() {
     let raw_optical_data = vec![vec1[1.0, 2.0], vec![3.0, 4.0], vec![5.0, 6.0]];
     let optical_data = process_optical(&raw_optical_data);
     println!("{:?}", optical_data);
 }
 
-pub fn process_optical(raw_optical_data: &Vec<f64>) -> Vec<f64> {
+
+
+pub fn process_optical(raw_optical_data: &Vec<Vec<f64>>) -> Vec<f64> {
 
     let mut optical_data = Vec::with_capacity(raw_optical_data.len());
 
@@ -18,8 +19,12 @@ pub fn process_optical(raw_optical_data: &Vec<f64>) -> Vec<f64> {
             magnitude += raw_optical_data[i][j].powi(2);
         }
 
+        optical_data.push(magnitude.sqrt());
     }
 
     optical_data
 
 }
+
+
+
