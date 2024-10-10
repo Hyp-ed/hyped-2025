952a1a9d97ba2eccf4673d13ff6ab733b9fbdb2b
Michael Belyaev <michael7belyaev@gmail.com>
2024-10-10 17:17:42 +0100

    fixes

--- lib/localisation/preprocessing/src/preprocess_keyence.rs
+++ lib/localisation/preprocessing/src/preprocess_keyence.rs
@@ -8,7 +8,7 @@ enum sensor_checks{
     unnaceptable,
 }
 
-fn main {
+fn main() {
     let keyence_data = vec![true, true, false, true, true];
     let sensor_check = check_keyence_agrees(&keyence_data);
     println!("{:?}", sensor_check);
@@ -16,24 +16,24 @@ fn main {
 
 pub fn check_keyence_agrees(keyence_data: &Vec<f64>) -> sensor_checks {
     let mut sensor_check = sensor_checks::acceptable;
-    let mut previous_data_status = keyence_data_status::agreed
-    let mut current_data_status = keyence_data_status::agreed
+    let mut previous_data_status = keyence_data_status::agreed;
+    let mut current_data_status = keyence_data_status::agreed;
 
     for i in 0..keyence_data.len()-1 {
         if keyence_data[i] != keyence_data{i+1} {
-            current_data_status = keyence_data_status::disagreed
+            current_data_status = keyence_data_status::disagreed;
         }
     }
 
     if current_data_status == keyence_data_status::disagreed && previous_data_status == keyence_data_status:: disagreed {
         println!("Keyence disagreement for two consecutive readings.");
 
-        sensor_check = sensor_checks::unnaceptable
+        sensor_check = sensor_checks::unnaceptable;
 
         sensor_check
     }
 
-    previous_data_status = current_data_status
+    previous_data_status = current_data_status;
 
     sensor_check
 }
