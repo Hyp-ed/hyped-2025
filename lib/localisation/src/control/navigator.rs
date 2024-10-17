4d832a7012d931362d85cd0bb0fc2a32626dad77
Michael Belyaev <michael7belyaev@gmail.com>
2024-10-17 18:27:04 +0100

    very basic implementaton

--- lib/localisation/src/control/navigator.rs
+++ lib/localisation/src/control/navigator.rs
@@ -1,1 +1,33 @@
-//commit test
+
+pub struct Nabigator {
+
+    displacement: f64,
+    velocity: f64,
+    acceleration: f64,
+
+}
+
+impl Navigator {
+
+    pub fn new() -> Navigator {
+        Navigator {
+            displacement: 0.0,
+            velocity: 0.0,
+            acceleration: 0.0,
+        }
+    }
+
+    pub fn set_displacement(&mut self, displacement: f64) {
+        self.displacement = displacement;
+    }
+
+    pub fn set_velocity(&mut self, velocity: f64) {
+        self.velocity = velocity;
+    }
+
+    pub fn set_acceleration(&mut self, acceleration: f64) {
+        self.acceleration = acceleration;
+    }
+
+
+}
