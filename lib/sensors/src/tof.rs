6a1c274932a02e7673973b3fabced498408f0e2e
Aux1r <ThePigZilla@gmail.com>
2024-10-17 15:37:42 +0100

    initial ToF code

--- lib/sensors/src/tof.rs
+++ lib/sensors/src/tof.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello World!");
+use hyped_io::i2c::HypedI2c;
+
+pub struct TimeOfFlight<T: HypedI2c> {
+    i2c: T,
+    device_address: u8,
+}
+
+impl<T: HypedI2c> TimeOfFlight<T> {
+    // Create a new instance of time of flight sensor, configure
+    pub fn new(mut i2c: T, device_address: ToFAddresses) -> Result<Self, ()> {
+        let device_address = device_address as u8;
+        // Set up the ToF sensor, sending config settings and setting mode to the appropriate registers
+
+    }
+}
+
+pub enum ToFAddresses {
+    Address29 = 0x29,
 }
