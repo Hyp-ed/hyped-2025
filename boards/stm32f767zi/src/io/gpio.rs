ab5815013d30857353917e05fbf081279c1a8ec4
David Beechey <david@dtbeechey.dev>
2024-10-18 13:34:40 +0100

    Rename `GpioPin` to `HypedGpioPin`

--- boards/stm32f767zi/src/io/gpio.rs
+++ boards/stm32f767zi/src/io/gpio.rs
@@ -1,12 +1,12 @@
 use embassy_stm32::gpio::Input;
-use hyped_io::gpio::GpioPin;
+use hyped_io::gpio::HypedGpioPin;
 
 /// A GPIO pin on the STM32L476RG.
 pub struct Stm32f767ziGpio {
     pin: Input<'static>,
 }
 
-impl GpioPin for Stm32f767ziGpio {
+impl HypedGpioPin for Stm32f767ziGpio {
     fn is_high(&mut self) -> bool {
         self.pin.is_high()
     }
--- boards/stm32l476rg/src/io/gpio.rs
+++ boards/stm32l476rg/src/io/gpio.rs
@@ -1,12 +1,12 @@
 use embassy_stm32::gpio::Input;
-use hyped_io::gpio::GpioPin;
+use hyped_io::gpio::HypedGpioPin;
 
 /// A GPIO pin on the STM32L476RG.
 pub struct Stm32l476rgGpio {
     pin: Input<'static>,
 }
 
-impl GpioPin for Stm32l476rgGpio {
+impl HypedGpioPin for Stm32l476rgGpio {
     fn is_high(&mut self) -> bool {
         self.pin.is_high()
     }
