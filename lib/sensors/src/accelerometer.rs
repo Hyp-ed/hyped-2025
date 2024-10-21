d7462142e13c290d934ab7d8a1d8fc2b7fdcdad0
miksquared <68959209+miksquared@users.noreply.github.com>
2024-10-21 19:22:40 +0100

    Add register addresses and values written to registers

--- lib/sensors/src/accelerometer.rs
+++ lib/sensors/src/accelerometer.rs
@@ -11,3 +11,23 @@ use hyped_io::i2c::{HypedI2c, I2cError};
 /// 
 /// Data sheet: https://www.st.com/resource/en/datasheet/lis2ds12.pdf
 /// Application notes: https://www.st.com/resource/en/application_note/an4748-lis2ds12-alwayson-3axis-accelerometer-stmicroelectronics.pdf
+
+
+// Registers for the LIS2DS12 accelerometer
+const LIS2DS12_CTRL1_ADDRESS: u8 = 0x20;
+const LIS2DS12_CTRL2_ADDRESS: u8 = 0x21;
+const LIS2DS12_FIFO_CTRL_ADDRESS: u8 = 0x25;
+
+const LIS2DS12_OUT_X_L: u8 = 0x28;
+const LIS2DS12_OUT_X_H: u8 = 0x29;
+const LIS2DS12_OUT_Y_L: u8 = 0x2A;
+const LIS2DS12_OUT_Y_H: u8 = 0x2B;
+const LIS2DS12_OUT_Z_L: u8 = 0x2C;
+const LIS2DS12_OUT_Z_H: u8 = 0x2D;
+
+const LIS2DS12_STATUS: u8 = 0x27;
+
+// Values written to control registers
+const LIS2DS12_CTRL1_VALUE: u8 = 0x54; // 200Hz, high performance, continuous, +-16g
+const LIS2DS12_CTRL2_VALUE: u8 = 0x0;
+const LIS2DS12_FIFO_CTRL_VALUE: u8 = 0x30;
