03a68645f58a252fcb6623ef2d44c63cf2d19f4b
DylanCavers <s2179041@ed.ac.uk>
2024-10-24 18:07:48 +0100

    add mqtt client in + small updates

--- lib/state_machine/src/state_machine.rs
+++ lib/state_machine/src/state_machine.rs
@@ -1,18 +1,46 @@
 use crate::types::{SourceAndTarget, State};
 use heapless::FnvIndexMap;
+use hyped_core::mqtt::HypedMqttClient;
+use embassy_net::tcp::TcpSocket;
+use rust_mqtt::utils::rng_generator::CountingRng;
 
-pub struct StateMachine {
+pub struct StateMachine<'a> {
     pub(crate) current_state: State,
     pub(crate) transition_map: FnvIndexMap<SourceAndTarget, State, 32>, // TODO: bring constant out
+    pub(crate) mqtt_client: HypedMqttClient<'a, TcpSocket<'a>, CountingRng>, 
 }
 
-impl StateMachine {
-    pub fn new() -> Self {
-        StateMachine {
+impl<'a> StateMachine<'a> {
+    pub fn new(
+        network_driver: TcpSocket<'a>,
+        buffer: &'a mut [u8],
+        buffer_len: usize,
+        recv_buffer: &'a mut [u8],
+        recv_buffer_len: usize,
+        client_id: &'a str,
+    ) -> Self {
+        let mut stm = StateMachine {
             current_state: State::Idle,
             transition_map: FnvIndexMap::<SourceAndTarget, State, 32>::new(),
-        }
-        // TODO: populate transition_map (idk if this can be done inplace which is annoying)
+            mqtt_client: HypedMqttClient::new(
+                network_driver, 
+                buffer,
+                buffer_len,
+                recv_buffer,
+                recv_buffer_len,
+                client_id,
+            ),
+        };
+        
+        // this is not a very nice way to do this, but it's a start (move out into a function)
+        let _ = stm.transition_map.insert(SourceAndTarget {
+            source: State::Idle,
+            target: State::Calibrate,
+        }, State::Calibrate);
+
+        // subscribe to topic
+
+        stm
     }
 
     pub fn handle_transition(&mut self, to_state: &State) {
@@ -25,9 +53,20 @@ impl StateMachine {
         }
     }
 
-    pub fn run(&mut self) {
+    pub async fn run(&mut self) {
         loop {
-            // TODO: consume, update, publish
+            // placeholder
+            let state = "idle";
+            self.publish_state("state", state.as_bytes()).await;
         }
+    }
+
+    pub async fn publish_state(&mut self, topic: &str, payload: &[u8]) {
+        self.mqtt_client.send_message(topic, payload, true).await;
+    }
+
+    pub async fn consume_state(&mut self) -> State {
+        let _msg = self.mqtt_client.receive_message().await;
+        State::Idle
     }
 }
