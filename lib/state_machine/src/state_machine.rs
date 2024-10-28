42282dee21478415940d0b1b594cc2744dbf2369
DylanCavers <s2179041@ed.ac.uk>
2024-10-28 19:42:38 +0000

    added state string conversions

--- lib/state_machine/src/state_machine.rs
+++ lib/state_machine/src/state_machine.rs
@@ -1,46 +1,33 @@
 use crate::types::{SourceAndTarget, State};
+use embassy_net::tcp::TcpSocket;
 use heapless::FnvIndexMap;
 use hyped_core::mqtt::HypedMqttClient;
-use embassy_net::tcp::TcpSocket;
 use rust_mqtt::utils::rng_generator::CountingRng;
 
 pub struct StateMachine<'a> {
     pub(crate) current_state: State,
-    pub(crate) transition_map: FnvIndexMap<SourceAndTarget, State, 32>, // TODO: bring constant out
-    pub(crate) mqtt_client: HypedMqttClient<'a, TcpSocket<'a>, CountingRng>, 
+    pub(crate) transition_map: FnvIndexMap<SourceAndTarget, State, 32>,
+    pub(crate) mqtt_client: HypedMqttClient<'a, TcpSocket<'a>, CountingRng>,
 }
 
 impl<'a> StateMachine<'a> {
-    pub fn new(
-        network_driver: TcpSocket<'a>,
-        buffer: &'a mut [u8],
-        buffer_len: usize,
-        recv_buffer: &'a mut [u8],
-        recv_buffer_len: usize,
-        client_id: &'a str,
-    ) -> Self {
-        let mut stm = StateMachine {
+    pub fn new(mqtt_client: HypedMqttClient<'a, TcpSocket<'a>, CountingRng>) -> Self {
+        let mut transition_map = FnvIndexMap::<SourceAndTarget, State, 32>::new();
+
+        // this is not a very nice way to do this, but it's a start (move out into a function consruct_transition_map)
+        let _ = transition_map.insert(
+            SourceAndTarget {
+                source: State::Idle,
+                target: State::Calibrate,
+            },
+            State::Calibrate,
+        );
+
+        StateMachine {
             current_state: State::Idle,
-            transition_map: FnvIndexMap::<SourceAndTarget, State, 32>::new(),
-            mqtt_client: HypedMqttClient::new(
-                network_driver, 
-                buffer,
-                buffer_len,
-                recv_buffer,
-                recv_buffer_len,
-                client_id,
-            ),
-        };
-        
-        // this is not a very nice way to do this, but it's a start (move out into a function)
-        let _ = stm.transition_map.insert(SourceAndTarget {
-            source: State::Idle,
-            target: State::Calibrate,
-        }, State::Calibrate);
-
-        // subscribe to topic
-
-        stm
+            transition_map,
+            mqtt_client,
+        }
     }
 
     pub fn handle_transition(&mut self, to_state: &State) {
@@ -54,10 +41,14 @@ impl<'a> StateMachine<'a> {
     }
 
     pub async fn run(&mut self) {
-        loop {
-            // placeholder
-            let state = "idle";
-            self.publish_state("state", state.as_bytes()).await;
+        self.mqtt_client.subscribe("stm").await;
+
+        while self.current_state != State::Shutdown {
+            let state = self.current_state.to_string();
+            self.publish_state("stm", state.as_bytes()).await;
+
+            let new_state = self.consume_state().await;
+            self.handle_transition(&new_state);
         }
     }
 
@@ -66,7 +57,7 @@ impl<'a> StateMachine<'a> {
     }
 
     pub async fn consume_state(&mut self) -> State {
-        let _msg = self.mqtt_client.receive_message().await;
-        State::Idle
+        let new_state = self.mqtt_client.receive_message().await.unwrap();
+        State::from_string(new_state.1).unwrap()
     }
 }
--- lib/state_machine/src/types.rs
+++ lib/state_machine/src/types.rs
@@ -1,3 +1,6 @@
+use core::str::FromStr;
+use heapless::String;
+
 #[derive(Hash, PartialEq, Eq, Clone, Copy)]
 pub enum State {
     Idle,
@@ -18,6 +21,55 @@ pub enum State {
     Failure,
     Safe,
     Shutdown,
+}
+
+impl State {
+    pub fn to_string(&self) -> String<20> {
+        match self {
+            State::Idle => String::<20>::from_str("idle").unwrap(),
+            State::Calibrate => String::<20>::from_str("calibrate").unwrap(),
+            State::Precharge => String::<20>::from_str("precharge").unwrap(),
+            State::ReadyForLevitation => String::<20>::from_str("ready_for_levitation").unwrap(),
+            State::BeginLevitation => String::<20>::from_str("begin_levitation").unwrap(),
+            State::Levitating => String::<20>::from_str("levitating").unwrap(),
+            State::Ready => String::<20>::from_str("ready").unwrap(),
+            State::Accelerate => String::<20>::from_str("accelerate").unwrap(),
+            State::LimBrake => String::<20>::from_str("lim_brake").unwrap(),
+            State::FrictionBrake => String::<20>::from_str("friction_brake").unwrap(),
+            State::StopLevitation => String::<20>::from_str("stop_levitation").unwrap(),
+            State::Stopped => String::<20>::from_str("stopped").unwrap(),
+            State::BatteryRecharge => String::<20>::from_str("battery_recharge").unwrap(),
+            State::CapacitorDischarge => String::<20>::from_str("capacitor_discharge").unwrap(),
+            State::FailureBrake => String::<20>::from_str("failure_brake").unwrap(),
+            State::Failure => String::<20>::from_str("failure").unwrap(),
+            State::Safe => String::<20>::from_str("safe").unwrap(),
+            State::Shutdown => String::<20>::from_str("shutdown").unwrap(),
+        }
+    }
+
+    pub fn from_string(state: &str) -> Option<State> {
+        match state {
+            "idle" => Some(State::Idle),
+            "calibrate" => Some(State::Calibrate),
+            "precharge" => Some(State::Precharge),
+            "ready_for_levitation" => Some(State::ReadyForLevitation),
+            "begin_levitation" => Some(State::BeginLevitation),
+            "levitating" => Some(State::Levitating),
+            "ready" => Some(State::Ready),
+            "accelerate" => Some(State::Accelerate),
+            "lim_brake" => Some(State::LimBrake),
+            "friction_brake" => Some(State::FrictionBrake),
+            "stop_levitation" => Some(State::StopLevitation),
+            "stopped" => Some(State::Stopped),
+            "battery_recharge" => Some(State::BatteryRecharge),
+            "capacitor_discharge" => Some(State::CapacitorDischarge),
+            "failure_brake" => Some(State::FailureBrake),
+            "failure" => Some(State::Failure),
+            "safe" => Some(State::Safe),
+            "shutdown" => Some(State::Shutdown),
+            _ => None,
+        }
+    }
 }
 
 #[derive(Hash, PartialEq, Eq)]
