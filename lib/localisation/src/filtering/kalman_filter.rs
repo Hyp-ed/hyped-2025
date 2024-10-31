6ccd14cdd067a40f4a2213da7492ccbbae6ceb3c
Michael Belyaev <michael7belyaev@gmail.com>
2024-10-31 19:39:02 +0000

    test

--- lib/localisation/src/filtering/kalman_filter.rs
+++ lib/localisation/src/filtering/kalman_filter.rs
@@ -44,7 +44,7 @@ impl KalmanFilter {
         }
     }
 
-    // Predict
+    // Predict step
     pub fn predict(&mut self, control_input: &DVector<f64>) {
         // x_k = F * x_k-1 + B * u_k
         self.state = &self.transition_matrix * &self.state + &self.control_matrix * control_input;
@@ -55,7 +55,7 @@ impl KalmanFilter {
                 + &self.process_noise;
     }
 
-    // Update
+    // Update step
     pub fn update(&mut self, measurement: &DVector<f64>) {
         // y_k = z_k - H * x_k
         let innovation = measurement - &self.observation_matrix * &self.state;
