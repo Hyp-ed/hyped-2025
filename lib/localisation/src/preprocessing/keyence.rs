77c2268aa433af23257319999dd0cdf38a48515b
lylebuist <102750153+lylebuist@users.noreply.github.com>
2024-11-04 13:45:53 +0000

    chagned struct to take new keyance data every run and added extra tests

--- lib/localisation/src/preprocessing/keyence.rs
+++ lib/localisation/src/preprocessing/keyence.rs
@@ -11,18 +11,17 @@ pub struct KeyenceAgrees {
 }
 
 impl KeyenceAgrees {
-    pub fn new(keyence_data: Vec<bool, 2>) -> Self {
+    pub fn new() -> Self {
         KeyenceAgrees {
-            keyence_data,
             previous_keyance_agreement: true,
         }
     }
 
     pub fn check_keyence_agrees(&mut self, keyence_data: Vec<bool, 2>) -> SensorChecks {
-        if self.keyence_data[0] != self.keyence_data[1] && !self.previous_keyance_agreement {
+        if keyence_data[0] != keyence_data[1] && !self.previous_keyance_agreement {
             return SensorChecks::Unnaceptable;
         } else {
-            self.previous_keyance_agreement = self.keyence_data[0] == self.keyence_data[1];
+            self.previous_keyance_agreement = keyence_data[0] == keyence_data[1];
         }
 
         SensorChecks::Acceptable
@@ -36,18 +35,55 @@ mod tests {
     #[test]
     fn test_acceptable_success() {
         let keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, true]).unwrap();
-        let mut keyence_agrees = KeyenceAgrees::new(keyence_data);
+        let mut keyence_agrees = KeyenceAgrees::new();
         let desired_outcome = SensorChecks::Acceptable;
-        let result = keyence_agrees.check_keyence_agrees();
+        let result = keyence_agrees.check_keyence_agrees(keyence_data);
         assert_eq!(result, desired_outcome);
     }
 
     #[test]
-    fn test_uncceptable_success() {
-        let keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, true]).unwrap();
-        let mut keyence_agrees = KeyenceAgrees::new(keyence_data);
+    fn test_acceptable_false_success() {
+        let keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, false]).unwrap();
+        let mut keyence_agrees = KeyenceAgrees::new();
         let desired_outcome = SensorChecks::Acceptable;
-        let result = keyence_agrees.check_keyence_agrees();
+        let result = keyence_agrees.check_keyence_agrees(keyence_data);
         assert_eq!(result, desired_outcome);
+    }
+
+    #[test]
+    fn test_acceptable_second_false_success() {
+        let first_keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, true]).unwrap();
+        let second_keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, false]).unwrap();
+        let mut keyence_agrees = KeyenceAgrees::new();
+        let desired_outcome = SensorChecks::Acceptable;
+        let initial_try = keyence_agrees.check_keyence_agrees(first_keyence_data);
+        let result = keyence_agrees.check_keyence_agrees(second_keyence_data);
+        assert_eq!(initial_try, desired_outcome);
+        assert_eq!(result, desired_outcome);
+    }
+
+    #[test]
+    fn test_acceptable_prev_false_success() {
+        let first_keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, false]).unwrap();
+        let second_keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, true]).unwrap();
+        let mut keyence_agrees = KeyenceAgrees::new();
+        let desired_outcome = SensorChecks::Acceptable;
+        let initial_try = keyence_agrees.check_keyence_agrees(first_keyence_data);
+        let result = keyence_agrees.check_keyence_agrees(second_keyence_data);
+        assert_eq!(initial_try, desired_outcome);
+        assert_eq!(result, desired_outcome);
+    }
+
+    #[test]
+    fn test_unnacceptable_prev_false_success() {
+        let first_keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, false]).unwrap();
+        let second_keyence_data: Vec<bool, 2> = Vec::from_slice(&[true, false]).unwrap();
+        let mut keyence_agrees = KeyenceAgrees::new();
+        let first_outcome = SensorChecks::Acceptable;
+        let second_outcome = SensorChecks::Unnaceptable;
+        let initial_try = keyence_agrees.check_keyence_agrees(first_keyence_data);
+        let result = keyence_agrees.check_keyence_agrees(second_keyence_data);
+        assert_eq!(initial_try, first_outcome);
+        assert_eq!(result, second_outcome);
     }
 }
