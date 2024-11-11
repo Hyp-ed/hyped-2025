3a9585e72c4f4e27578f4bff2c24189da9bf2756
Michael Belyaev <michael7belyaev@gmail.com>
2024-11-11 19:13:46 +0000

    getters & setters

--- lib/localisation/src/control/navigator.rs
+++ lib/localisation/src/control/navigator.rs
@@ -1,1 +1,48 @@
-//test
+use crate::filtering::kalman_filter::KalmanFilter;
+
+
+pub struct Navigator {
+    displacement: f64,
+    velocity: f64,
+    acceleration: f64,
+    kalman_filter: KalmanFilter,
+}
+
+impl Navigator {
+    pub fn new(kalman_filter: KalmanFilter) -> Navigator {
+        Navigator {
+            displacement: 0.0,
+            velocity: 0.0,
+            acceleration: 0.0,
+            kalman_filter,
+        }
+    }
+
+    //Setters
+
+    pub fn set_displacement(&mut self, displacement: f64) {
+        self.displacement = displacement;
+    }
+
+    pub fn set_velocity(&mut self, velocity: f64) {
+        self.velocity = velocity;
+    }
+
+    pub fn set_acceleration(&mut self, acceleration: f64) {
+        self.acceleration = acceleration;
+    }
+
+    //Getters
+
+    pub fn get_displacement(&self) -> f64 {
+        self.displacement
+    }
+
+    pub fn get_velocity(&self) -> f64 {
+        self.velocity
+    }
+
+    pub fn get_acceleration(&self) -> f64 {
+        self.acceleration
+    }
+}
