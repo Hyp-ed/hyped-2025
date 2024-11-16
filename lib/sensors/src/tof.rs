db47381951fedeb02cdb435ee1e9aceb2a5bab87
Aux1r <ThePigZilla@gmail.com>
2024-11-16 13:54:14 +0000

    finding the bad u8

--- lib/sensors/src/tof.rs
+++ lib/sensors/src/tof.rs
@@ -22,16 +22,6 @@ impl<'a, T: HypedI2c> TimeOfFlight<'a, T> {
     pub fn new(i2c: &'a mut T, device_address: ToFAddresses) -> Result<Self, ToFError> {
         // SR03 Settings as seen in Application Sheet
         let device_address = device_address as u8;
-        for (reg, val) in PRIVATE_REGISTERS_u8 {
-            // writing to private registers
-            if let Err(e) = i2c.write_byte_to_register(
-                device_address,
-                reg,
-                val,
-            ) {
-                panic!("Error writing private registers u8s");
-            }
-        }
         for (reg,val) in PRIVATE_REGISTERS_u16 {
             // writing to private registers u16
             if let Err(e) = i2c.write_byte_to_register_16(
@@ -243,17 +233,10 @@ mod tests {
         let i2c_values = FnvIndexMap::new();
         let mut i2c = MockI2c::new(i2c_values);
         let _ = TimeOfFlight::new(&mut i2c, ToFAddresses::Address29);
-        for (reg,val) in PRIVATE_REGISTERS_u8 {
-            assert_eq!(
-                i2c.get_writes()
-                    .get(&(ToFAddresses::Address29 as u8, reg.into())),
-                Some(&Some(val))
-            )
-        }
         for (reg,val) in PRIVATE_REGISTERS_u16 {
             assert_eq!(
                 i2c.get_writes()
-                    .get(&(ToFAddresses::Address29 as u8, reg.into())),
+                    .get(&(ToFAddresses::Address29 as u8, reg)),
                 Some(&Some(val))
             )
         }
