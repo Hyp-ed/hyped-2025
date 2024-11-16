5a16e61980083a83e8920754f21eaf608934bd9f
H00pyFr00d <kiff2002@gmail.com>
2024-11-16 14:57:15 +0000

    Updated comments and removed dead code

--- lib/io/src/adc_mux.rs
+++ lib/io/src/adc_mux.rs
@@ -1,7 +1,6 @@
 use hyped_io::i2c::{HypedI2c, I2cError};
 
-/// ADCMux implements logic to read a specified number of channel registers on the ADC Mux
-/// and report any failures
+/// ADCMux implements logic to read a channel on the ADC Mux and report any failures
 ///
 /// Data sheet: https://www.ti.com/lit/ds/symlink/adc128d818.pdf
 pub struct ADCMuxChannel<T: HypedI2c> {
@@ -13,7 +12,6 @@ pub struct ADCMuxChannel<T: HypedI2c> {
 
 impl<T: HypedI2c> ADCMuxChannel<T> {
     /// Create a new instance of the ADC mux sensor and configure it
-    /// num_channels: number of channels to use (up to 8 max)
     pub fn new(mut i2c: T, device_address: u8, channel: AdcChannelAddress) -> Result<Self, MuxError> {
         Ok(Self {
             i2c,
@@ -33,22 +31,20 @@ impl HypedAdc for ADCMuxChannel<I2c> {
 /// ADC Mux addresses
 const ADC_MUX_ADDRESS: u8 = 0x1D;
 pub enum AdcChannelAddress {
-    AdcChannel20 = 0x20, 
-    AdcChannel21 = 0x21, 
-    AdcChannel22 = 0x22, 
-    AdcChannel23 = 0x23, 
-    AdcChannel24 = 0x24, 
-    AdcChannel25 = 0x25, 
-    AdcChannel26 = 0x26, 
-    AdcChannel27 = 0x27,
+    AdcChannel0 = 0x20, 
+    AdcChannel1 = 0x21, 
+    AdcChannel2 = 0x22, 
+    AdcChannel3 = 0x23, 
+    AdcChannel4 = 0x24, 
+    AdcChannel5 = 0x25, 
+    AdcChannel6 = 0x26, 
+    AdcChannel7 = 0x27,
 }
 
 /// Possible errors that can occur when interacting with the ADC Mux
 #[derive(Debug)]
 pub enum MuxError {
     I2cError(I2cError),
-    TooManyFaultyChannels,
-    TooManyChannels,
 }
 
 // Values for error checking
