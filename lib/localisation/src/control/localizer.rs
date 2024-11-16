24a43c2856f68c8f290b54a5fc31a0be299c57d9
Michael Belyaev <michael7belyaev@gmail.com>
2024-11-16 14:46:29 +0000

    initialised kalman filter

--- lib/localisation/src/control/localizer.rs
+++ lib/localisation/src/control/localizer.rs
@@ -1,6 +1,9 @@
+use nalgebra::{Matrix2, Vector2};
+use libm::pow;
 use crate::filtering::kalman_filter::KalmanFilter;
 
 
+const DELTA_T: f64 = 0.01;
 
 pub struct Localizer {
     displacement: f64,
@@ -11,6 +14,42 @@ pub struct Localizer {
 
 impl Localizer {
     pub fn new(kalman_filter: KalmanFilter) -> Localizer {
+
+       
+
+        let initial_state = Vector2::new(0.0, 0.0);
+        let initial_covariance = Matrix2::new(1.0, 0.0, 0.0, 1.0);
+        let transition_matrix = Matrix2::new(1.0, DELTA_T, 0.0, 1.0);
+        let control_matrix = Vector2::new(0.5 * DELTA_T * DELTA_T, DELTA_T);
+        let observation_matrix = Matrix2::new(1.0, 0.0, 0.0, DELTA_T);
+        
+        // Assuming frequency of 6400hz for IMU at 120 mu g / sqrt(Hz)
+        // standard deviation = 120 * sqrt(6400) = 9600 mu g = 0.0096 g
+        //                    = 0.0096 * 9.81 = 0.094176 m/s^2
+        // variance =  0.094176^2 = 0.0089 m/s^2
+
+        let process_noise: Matrix2<f64> = Matrix2::new(
+          0.25 * pow(DELTA_T,4.0), 0.5 * pow(DELTA_T,3.0),
+            0.5 * pow(DELTA_T,3.0), pow(DELTA_T,2.0) * 0.0089);
+        
+        //  We assume the stripe counter is accurate
+        //  Optical flow expects standard deviation of 0.01% of the measured value
+        //  Assuming top speed 10m/s,
+        //  standard deviation = 0.01 * 10 = 0.1 m/s
+        //  variance = 0.1^2 = 0.01 m/s^2
+
+        let measurement_noise: Matrix2<f64> = Matrix2::new(0.01, 0.0, 0.0, 0.0);
+        
+        let kalman_filter = KalmanFilter::new(
+            initial_state,
+            initial_covariance,
+            transition_matrix,
+            control_matrix,
+            observation_matrix,
+            process_noise,
+            measurement_noise,
+        );
+
         Localizer {
             displacement: 0.0,
             velocity: 0.0,
--- lib/localisation/src/lib.rs
+++ lib/localisation/src/lib.rs
@@ -1,3 +1,4 @@
 #![no_std]
 pub mod control;
 pub mod filtering;
+
