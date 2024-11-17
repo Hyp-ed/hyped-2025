8823cc74d6df9ada22d038e9345a1c0d49ecbc82
David Beechey <david@dtbeechey.dev>
2024-11-17 14:40:48 +0000

    fix issues with ADC Mux implementation

--- lib/io/src/adc_mux.rs
+++ lib/io/src/adc_mux.rs
@@ -1,4 +1,7 @@
-use hyped_io::i2c::{HypedI2c, I2cError};
+use crate::{
+    adc::HypedAdc,
+    i2c::{HypedI2c, I2cError},
+};
 
 /// ADCMux implements logic to read a channel on the ADC Mux and report any failures
 ///
@@ -9,10 +12,9 @@ pub struct ADCMuxChannel<T: HypedI2c> {
     channel: AdcChannelAddress,
 }
 
-
 impl<T: HypedI2c> ADCMuxChannel<T> {
     /// Create a new instance of the ADC mux sensor and configure it
-    pub fn new(mut i2c: T, device_address: u8, channel: AdcChannelAddress) -> Result<Self, MuxError> {
+    pub fn new(i2c: T, device_address: u8, channel: AdcChannelAddress) -> Result<Self, MuxError> {
         Ok(Self {
             i2c,
             device_address,
@@ -21,23 +23,28 @@ impl<T: HypedI2c> ADCMuxChannel<T> {
     }
 }
 
-impl HypedAdc for ADCMuxChannel<I2c> {
+impl<T: HypedI2c> HypedAdc for ADCMuxChannel<T> {
     /// Read a value from the ADC Mux channel
     fn read_value(&mut self) -> u16 {
-        self.i2c.read_byte(self.device_address, ADC_MUX_ADDRESS).unwrap() as u16
+        self.i2c
+            .read_byte(self.device_address, self.channel as u8)
+            .unwrap() as u16
     }
 }
 
-/// ADC Mux addresses
-const ADC_MUX_ADDRESS: u8 = 0x1D;
+pub const ADC_MUX_ADDRESS: u8 = 0x1D;
+
+/// ADC Mux channel addresses
+#[repr(u8)]
+#[derive(Clone, Copy)]
 pub enum AdcChannelAddress {
-    AdcChannel0 = 0x20, 
-    AdcChannel1 = 0x21, 
-    AdcChannel2 = 0x22, 
-    AdcChannel3 = 0x23, 
-    AdcChannel4 = 0x24, 
-    AdcChannel5 = 0x25, 
-    AdcChannel6 = 0x26, 
+    AdcChannel0 = 0x20,
+    AdcChannel1 = 0x21,
+    AdcChannel2 = 0x22,
+    AdcChannel3 = 0x23,
+    AdcChannel4 = 0x24,
+    AdcChannel5 = 0x25,
+    AdcChannel6 = 0x26,
     AdcChannel7 = 0x27,
 }
 
@@ -46,7 +53,3 @@ pub enum AdcChannelAddress {
 pub enum MuxError {
     I2cError(I2cError),
 }
-
-// Values for error checking
-const FAILURE_THRESHOLD: f32 = 0.25;
-const MAX_MUX_CHANNELS: usize = 8;
