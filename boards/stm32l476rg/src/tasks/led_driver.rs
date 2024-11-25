c7bb2bd860f9820fee76f9433229f74b8e638ff0
SinY/CosY <elitan.uk@gmail.com>
2024-11-25 19:11:15 +0000

    Created LED driver task

---
+++ boards/stm32l476rg/src/tasks/led_driver.rs
@@ -0,0 +1,34 @@
+use crate::io::i2c::Stm32l476rgI2c;
+use defmt_rtt as _;
+use embassy_stm32::i2c::I2c;
+use embassy_stm32::time::Hertz;
+use hyped_sensors::LedDriver::{LedDriver, LedDriverAddresses};
+
+#[embassy_executor::task]
+pub async fn write_led() -> ! {
+    let p = embassy_stm32::init(Default::default());
+    let i2c = I2c::new_blocking(p.I2C1, p.PB8, p.PB9, Hertz(100_000), Default::default());
+    let mut hyped_i2c = Stm32l476rgI2c::new(i2c);
+
+    let mut led_driver_sensor = LedDriver::new(&mut hyped_i2c, LedDriverAddresses::Address30)
+        .expect(
+            "Failed to create LED driver instance. Check wiring and I2C address of component.",
+    );
+
+    const DRIVER_ADDRESSES: [u8; 8] = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF];
+    let mut index = 0;
+
+    loop {
+        if (index == 7){
+            match led_driver_sensor.reset(){
+                OK(_) => (),
+                Err(e) => "Failed to reset LED driver"
+            };
+        }
+        match led_driver_sensor.set_led_colour(LED_CONFIG0, DRIVER_ADDRESSES[index], 0xFF, 0xFF, 0xFF, 0xFF){
+            OK(_) => (),
+            Err(e) => "Failed to set colour of LED " + DRIVER_ADDRESSES[index]
+        };
+        index += 1;
+    }
+}
