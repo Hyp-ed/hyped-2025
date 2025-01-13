8f8f56f6d72df30e57367c24f6c3b69812079a8c
SinY/CosY <elitan.uk@gmail.com>
2025-01-13 19:20:30 +0000

    fix formatting errors

--- boards/stm32l476rg/src/tasks/led_driver.rs
+++ boards/stm32l476rg/src/tasks/led_driver.rs
@@ -11,14 +11,15 @@ pub async fn write_led() -> ! {
     let mut hyped_i2c = Stm32l476rgI2c::new(i2c);
 
     let mut led_driver_sensor = LedDriver::new(&mut hyped_i2c, LedDriverAddresses::Address30)
-        .expect("Failed to create LED driver instance. Check wiring and I2C address of component.",);
+        .expect("Failed to create LED driver instance. Check wiring and I2C address of component.");
 
     const DRIVER_ADDRESSES: [u8; 8] = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F, 0xFF];
     let mut index = 0;
 
     loop {
         // flash each LED in sequence
-        if index == 8 { // reset LED Driver after flashing 8th test LED
+        if index == 8 {
+            // reset LED Driver after flashing 8th test LED
             match led_driver_sensor.reset(){
                 Ok(_) => (),
                 Err(_) => {
@@ -26,7 +27,13 @@ pub async fn write_led() -> ! {
                 }
             };
         }
-        match led_driver_sensor.set_led_colour(LED_CONFIG0, DRIVER_ADDRESSES[index], 0xFF, 0xFF, 0xFF, 0xFF){
+        match led_driver_sensor.set_led_colour(
+            LED_CONFIG0,
+            DRIVER_ADDRESSES[index],
+            0xFF, 
+            0xFF, 
+            0xFF, 
+            0xFF){
             Ok(_) => (),
             Err(_) => {
                 defmtt::error!("Failed to set colour of LED.");
--- lib/sensors/src/led_driver.rs
+++ lib/sensors/src/led_driver.rs
@@ -1,16 +1,16 @@
 use hyped_io::i2c::{HypedI2c, I2cError};
 
 /// LedDriver implements logic and required registers and addresses from the LP503x LED driver
-/// 
+
 /// LED driver is set to colour bank control mode, LED colour and brightness can only be set
 /// for those in bank control mode.
 /// All LEDs in bank control mode share the same colour and brightness changes unless set
 /// off bank control mode
-/// 
+
 /// When setting colours and brightness of LEDs, use the set_led_colours() function.
 ///     LEDs needing to be set are controlled by the addresses in either LED_CONFIG0 or LED_CONFIG1 registers
 ///     These addresses can be toggled with hexadecimal (E.G: registers 11101000 are set by the hex value E8)
-///
+
 /// link to data sheet (https://www.ti.com/lit/ds/symlink/lp5036.pdf?HQS=dis-mous-null-mousermode-dsf-pf-null-wwe&ts=1698441544495&ref_url=https%253A%252F%252Fwww.mouser.co.uk%252F)
 
 pub struct LedDriver<'a, T: HypedI2c + 'a> {
