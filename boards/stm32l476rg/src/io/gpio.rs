52884d03d0dd933f5920c3115dbd600509b2f32e
jan <janokul92@gmail.com>
2025-01-13 18:59:11 +0000

    Anything

--- boards/stm32l476rg/src/io/gpio.rs
+++ boards/stm32l476rg/src/io/gpio.rs
@@ -1,12 +1,12 @@
 use embassy_stm32::gpio::Input;
-use hyped_io::gpio::GpioPin;
+use hyped_io::gpio::GpioInputPin;
 
 /// A GPIO pin on the STM32L476RG.
 pub struct Stm32l476rgGpio {
     pin: Input<'static>,
 }
 
-impl GpioInputPinPin for Stm32l476rgGpio {
+impl GpioInputPin for Stm32l476rgGpio {
     fn is_high(&mut self) -> bool {
         self.pin.is_high()
     }
