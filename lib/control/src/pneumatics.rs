ddef6576b3b5647870736bbfa74f122d12eb4861
David Beechey <david@dtbeechey.dev>
2025-01-14 17:36:01 +0000

    Add documentation and fix crate include

--- lib/control/src/pneumatics.rs
+++ lib/control/src/pneumatics.rs
@@ -1,4 +1,4 @@
-use hyped_io::gpio::GpioOutputPin;
+use hyped_gpio_input::GpioOutputPin;
 
 pub enum BrakeState {
     Engaged,
@@ -10,6 +10,8 @@ pub enum LateralSuspensionState {
     Retracted,
 }
 
+/// Represents the pneumatics systems (brakes and lateral suspension) of the pod.
+/// Outputs two GPIO signals, one for the brakes and one for the lateral suspension, which turn on/off a solenoid valve.
 pub struct Pneumatics<P: GpioOutputPin> {
     brakes: BrakeState,
     lateral_suspension: LateralSuspensionState,
@@ -26,11 +28,13 @@ impl<P: GpioOutputPin> Pneumatics<P> {
             lateral_suspension_pin,
         };
 
+        // Engage brakes and retract lateral suspension on startup
         pneumatics.engage_brakes();
         pneumatics.retract_lateral_suspension();
         pneumatics
     }
 
+    /// Engages the brakes by setting the brake GPIO pin to low.
     pub fn engage_brakes(&mut self) {
         self.brakes = BrakeState::Engaged;
 
@@ -39,6 +43,7 @@ impl<P: GpioOutputPin> Pneumatics<P> {
         self.brake_pin.set_low();
     }
 
+    /// Disengages the brakes by setting the brake GPIO pin to high.
     pub fn disengage_brakes(&mut self) {
         self.brakes = BrakeState::Disengaged;
 
@@ -47,14 +52,16 @@ impl<P: GpioOutputPin> Pneumatics<P> {
         self.brake_pin.set_high();
     }
 
+    /// Deploys the lateral suspension by setting the lateral suspension GPIO pin to high.
     pub fn deploy_lateral_suspension(&mut self) {
-        self.lateral_suspension = LateralSuspensionState::Deployed;
         self.lateral_suspension_pin.set_high();
+        self.lateral_suspension = LateralSuspensionState::Deployed;
     }
 
+    /// Retracts the lateral suspension by setting the lateral suspension GPIO pin to low.
     pub fn retract_lateral_suspension(&mut self) {
-        self.lateral_suspension = LateralSuspensionState::Retracted;
         self.lateral_suspension_pin.set_low();
+        self.lateral_suspension = LateralSuspensionState::Retracted;
     }
 
     pub fn get_brake_state(self) -> BrakeState {
