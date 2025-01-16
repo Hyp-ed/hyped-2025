3a929155222ff3dd59ecaca38eaf164000544a26
Aux1r <ThePigZilla@gmail.com>
2025-01-16 18:30:29 +0000

    code adjustments for new i2c

--- boards/stm32l476rg/src/tasks/time_of_flight.rs
+++ boards/stm32l476rg/src/tasks/time_of_flight.rs
@@ -1,14 +1,22 @@
-use crate::io::i2c::Stm32l476rgI2c;
+use crate::io::Stm32l476rgI2c;
+use core::cell::RefCell;
 use defmt_rtt as _;
 use embassy_stm32::i2c::I2c;
 use embassy_stm32::time::Hertz;
+use embassy_sync::blocking_mutex::Mutex;
 use hyped_sensors::time_of_flight::{TimeOfFlight, TimeOfFlightAddresses};
 
 // Test task that continuously starts a single shot measurement and reads the result
 #[embassy_executor::task]
 pub async fn read_time_of_flight_range() -> ! {
     let p = embassy_stm32::init(Default::default());
-    let i2c = I2c::new_blocking(p.I2C1, p.PB8, p.PB9, Hertz(100_000), Default::default());
+    let i2c = Mutex::new(RefCell::new(I2c::new_blocking(
+        p.I2C1,
+        p.PB8,
+        p.PB9,
+        Hertz(100_000),
+        Default::default(),
+    )));
     let mut hyped_i2c = Stm32l476rgI2c::new(i2c);
 
     let mut time_of_flight_sensor = TimeOfFlight::new(
@@ -20,20 +28,14 @@ pub async fn read_time_of_flight_range() -> ! {
     );
 
     loop {
-        // defmt::info!("Starting single shot measurement");
-        // defmt::info!("Polling for range");
-        time_of_flight_sensor.start_single_shot_measurement();
-        // defmt::info!("Started single shot measurement");
-
-        match time_of_flight_sensor.read_range() {
-            Some(range) => {
-                defmt::info!("Range: {:?} mm", range)
+        match time_of_flight_sensor.single_shot_measurement() {
+            Ok(range) => {
+                defmt::info!("Range {:?} mm", range)
             }
-            None => {
-                defmt::info!("Failed to read range")
+            Err(e) => {
+                panic!("{:?}", e)
             }
         }
         time_of_flight_sensor.clear_interrupts();
-        // defmt::info!("Cleared Interrupts!")
     }
 }
