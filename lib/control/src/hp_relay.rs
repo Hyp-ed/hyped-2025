34f87033a519c7977d615764c456a3a49e99f96c
David Beechey <david@dtbeechey.dev>
2025-01-21 18:30:21 +0000

    Add documentation

--- lib/control/src/hp_relay.rs
+++ lib/control/src/hp_relay.rs
@@ -1,8 +1,6 @@
-// need to take in a GPIO pin like Keyence does
-// functions: switch_on, switch_off
-
 use hyped_gpio::HypedGpioOutputPin;
 
+/// Controls the high power relay by switching it on and off using a GPIO pin.
 pub struct HighPowerRelay<T: HypedGpioOutputPin> {
     gpio: T,
 }
@@ -12,10 +10,12 @@ impl<T: HypedGpioOutputPin> HighPowerRelay<T> {
         HighPowerRelay { gpio }
     }
 
+    /// Switches the relay on by setting the GPIO pin high.
     pub fn switch_on(&mut self) {
         self.gpio.set_high();
     }
 
+    /// Switches the relay off by setting the GPIO pin low.
     pub fn switch_off(&mut self) {
         self.gpio.set_low();
     }
