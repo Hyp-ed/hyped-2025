bcc58afaae2b3f957d006cf598c89ebb2bd93c04
jpfbastos <jpfbastos05@gmail.com>
2025-01-23 19:18:54 +0000

    rustfmt

--- lib/io/hyped_can/src/lib.rs
+++ lib/io/hyped_can/src/lib.rs
@@ -20,8 +20,8 @@ pub enum CanError {
 }
 #[derive(Clone)]
 pub struct CanFrame {
-    can_id: u32,                        // 32 bit CAN_ID + EFF/RTR/ERR flags
-    data: [u8; 8],                      // data that is sent over CAN, split into bytes
+    can_id: u32,   // 32 bit CAN_ID + EFF/RTR/ERR flags
+    data: [u8; 8], // data that is sent over CAN, split into bytes
 }
 
 /// CAN trait used to abstract the CAN operations
@@ -52,39 +52,42 @@ pub mod mock_can {
         /// Read a frame from the CAN bus
         async fn read_frame(&mut self) -> Option<u8> {
             self.frames_to_read.lock(|frames_to_read| {
-                frames_to_read.borrow_mut().pop_front().map(|frame| frame.data[0])
+                frames_to_read
+                    .borrow_mut()
+                    .pop_front()
+                    .map(|frame| frame.data[0])
             })
         }
-    
+
         fn try_read_frame(&mut self) -> Option<u8> {
-            self.frames_to_read.lock(|frames_to_read| {
-                frames_to_read.borrow().front().map(|frame| frame.data[0])
-            })
+            self.frames_to_read
+                .lock(|frames_to_read| frames_to_read.borrow().front().map(|frame| frame.data[0]))
         }
-    
+
         /// Write a CAN frame to the CAN bus
         async fn write_frame(&mut self, message: &super::CanFrame) -> Result<(), super::CanError> {
-            
             match self.frames_sent.push_front(message.clone()) {
                 Ok(_) => Ok(()),
                 Err(_) => Err(super::CanError::Unknown),
             }
         }
-    
+
         fn try_write_frame(&mut self, message: &super::CanFrame) -> Result<(), super::CanError> {
             match self.frames_sent.push_front(message.clone()) {
                 Ok(_) => Ok(()),
                 Err(_) => Err(super::CanError::Unknown),
             }
         }
-    
     }
-    
+
     impl MockCan<'_> {
-        pub fn new(frames_to_read: &'static Mutex<CriticalSectionRawMutex, RefCell<CanValues>>) -> Self {
-            MockCan { 
+        pub fn new(
+            frames_to_read: &'static Mutex<CriticalSectionRawMutex, RefCell<CanValues>>,
+        ) -> Self {
+            MockCan {
                 frames_to_read,
-                frames_sent: CanValues::new(),}
+                frames_sent: CanValues::new(),
+            }
         }
         /// Get the values that have been sent over the CAN bus
         pub fn get_can(&self) -> &CanValues {
