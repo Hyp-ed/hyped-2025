eebeeec8cd40f3b0beaa91fa76d1a2ca61d393fa
Archie Spiller <83834735+platinumxy@users.noreply.github.com>
2025-01-24 00:02:38 +0000

    More CAN sendable stuff, in a place holder location for now

--- lib/core/src/can_sendable.rs
+++ lib/core/src/can_sendable.rs
@@ -1,6 +1,26 @@
+use defmt::error;
+
 pub const CAN_MSG_TYPE_BOOL: u8 = 0;
 pub const CAN_MSG_TYPE_F32 : u8 = 1;
 pub const CAN_MSG_TYPE_U32 : u8 = 2;
+
+pub enum CanMsgType {
+    Bool,
+    F32,
+    U32,   
+}
+
+pub fn can_msg_type_from_u8(msg_type: &[u8; 8]) -> CanMsgType {
+    match msg_type[0] & 0x0F {
+        CAN_MSG_TYPE_BOOL => CanMsgType::Bool,
+        CAN_MSG_TYPE_F32 => CanMsgType::F32,
+        CAN_MSG_TYPE_U32 => CanMsgType::U32,
+        _ => {
+            error!("Unknown CAN message type: {}", msg_type[0] & 0x0F);
+            panic!();
+        },
+    }
+}
 
 pub trait CanSendable { 
     fn encode_to_can(&self, board_id:u8) -> [u8; 8];
@@ -45,5 +65,39 @@ impl CanSendable for f32 {
         let f32_bytes: [u8; 4] = self.to_le_bytes();
         data[1..5].copy_from_slice(&f32_bytes);
         return data;
+    }
+}
+
+pub struct PositionDelta {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+}
+
+impl Default for PositionDelta {
+    fn default() -> Self {
+        PositionDelta {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+}
+
+impl PositionDelta {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        PositionDelta {
+            x,
+            y,
+            z,
+        }
+    }
+
+    pub fn encode_to_can(&self, board_id:u8) -> [[u8; 8]; 3] {
+        [
+            self.x.encode_to_can(board_id),
+            self.y.encode_to_can(board_id),
+            self.z.encode_to_can(board_id),
+        ]
     }
 }
