8d55d2bb28f17ae8d372ae92fada4c8601f89a18
jpfbastos <jpfbastos05@gmail.com>
2025-01-30 19:57:10 +0000

    scrapped async because they're a pain

--- lib/io/hyped_can/hyped_can_derive/src/lib.rs
+++ lib/io/hyped_can/hyped_can_derive/src/lib.rs
@@ -15,15 +15,28 @@ fn impl_hyped_can(ast: &syn::DeriveInput) -> TokenStream {
     let generics: &Generics = &ast.generics;
     let (impl_generics, ty_generics, _) = generics.split_for_impl();
     let gen = quote! {
-        impl #impl_generics HypedCan for #name #ty_generics{
-
-            async fn read_frame(&mut self) -> Result<Envelope, CanError> {
-                let result = self.can.lock(|can| {
-                    can.borrow_mut().read()
-                });
+        impl #impl_generics HypedCan for #name #ty_generics {
+            fn read_frame(&mut self) -> Result<HypedEnvelope, CanError> {
+                let result = self.can.lock(|can| can.borrow_mut().try_read());
                 match result {
-                    Ok(_) => Ok(result),
-                    Err(e) => Err(match e {
+                    Ok(envelope) => Ok(HypedEnvelope {
+                        frame: CanFrame {
+                            can_id: {
+                                let id = envelope.frame.id();
+                                match id {
+                                    embassy_stm32::can::Id::Standard(id) => id.as_raw() as u32, // 11-bit ID
+                                    embassy_stm32::can::Id::Extended(id) => id.as_raw(),        // 29-bit ID
+                                }
+                            },
+                            data: {
+                                let mut data = [0u8; 8];
+                                data.copy_from_slice(envelope.frame.data());
+                                data
+                            },
+                        },
+                        ts: envelope.ts,
+                    }),
+                    Err(embassy_stm32::can::enums::TryReadError::BusError(e)) => Err(match e {
                         embassy_stm32::can::enums::BusError::Stuff => CanError::Stuff,
                         embassy_stm32::can::enums::BusError::Form => CanError::Form,
                         embassy_stm32::can::enums::BusError::Acknowledge => CanError::Acknowledge,
@@ -35,59 +48,51 @@ fn impl_hyped_can(ast: &syn::DeriveInput) -> TokenStream {
                         embassy_stm32::can::enums::BusError::BusPassive => CanError::BusPassive,
                         embassy_stm32::can::enums::BusError::BusWarning => CanError::BusWarning,
                     }),
+                    Err(embassy_stm32::can::enums::TryReadError::Empty) => Err(CanError::Empty),
                 }
             }
 
-            fn try_read_frame(&mut self) -> Result<Envelope, CanError> {
-                let result = self.can.lock(|can| {
-                    can.borrow().try_read()
-                });
-                match result {
-                    Ok(_) => Ok(result),
+            fn write_frame(&mut self, frame: &CanFrame) -> Result<(), CanError> {
+                let frame = embassy_stm32::can::Frame::new(
+                    match frame.can_id {
+                        id if id <= 0x7FF => embassy_stm32::can::frame::Header::new(
+                            embassy_stm32::can::Id::Standard(
+                                embassy_stm32::can::StandardId::new(id as u16).unwrap(),
+                            ),
+                            frame.data.len() as u8,
+                            false,
+                        ),
+                        id => embassy_stm32::can::frame::Header::new(
+                            embassy_stm32::can::Id::Extended(
+                                embassy_stm32::can::ExtendedId::new(id).unwrap(),
+                            ),
+                            frame.data.len() as u8,
+                            false,
+                        ),
+                    },
+                    &frame.data,
+                );
+                match frame {
+                    Ok(frame) => {
+                        let result = self.can.lock(|can| can.borrow_mut().try_write(&frame));
+                        match result {
+                            Ok(_) => Ok(()),
+                            Err(embassy_stm32::can::TryWriteError::Full) => Err(CanError::Full),
+                        }
+                    }
                     Err(e) => Err(match e {
-                        embassy_stm32::can::enums::BusError::Stuff => CanError::Stuff,
-                        embassy_stm32::can::enums::BusError::Form => CanError::Form,
-                        embassy_stm32::can::enums::BusError::Acknowledge => CanError::Acknowledge,
-                        embassy_stm32::can::enums::BusError::BitRecessive => CanError::BitRecessive,
-                        embassy_stm32::can::enums::BusError::BitDominant => CanError::BitDominant,
-                        embassy_stm32::can::enums::BusError::Crc => CanError::Crc,
-                        embassy_stm32::can::enums::BusError::Software => CanError::Software,
-                        embassy_stm32::can::enums::BusError::BusOff => CanError::BusOff,
-                        embassy_stm32::can::enums::BusError::BusPassive => CanError::BusPassive,
-                        embassy_stm32::can::enums::BusError::BusWarning => CanError::BusWarning,
-                    }),
-                }
-            }
-
-            async fn write_frame(&mut self, frame: &CanFrame) {
-
-                self.can.lock(|can| {
-                    can.borrow_mut().write(frame)
-                });
-            }
-
-            fn try_write_frame(&mut self, frame: &CanFrame) -> Result<(), CanError> {
-
-                let result = self.can.lock(|can| {
-                    can.borrow_mut().try_write(frame)
-                });
-                match result {
-                    Ok(_) => Ok(()),
-                    Err(e) => Err(match e {
-                        embassy_stm32::can::enums::BusError::Stuff => CanError::Stuff,
-                        embassy_stm32::can::enums::BusError::Form => CanError::Form,
-                        embassy_stm32::can::enums::BusError::Acknowledge => CanError::Acknowledge,
-                        embassy_stm32::can::enums::BusError::BitRecessive => CanError::BitRecessive,
-                        embassy_stm32::can::enums::BusError::BitDominant => CanError::BitDominant,
-                        embassy_stm32::can::enums::BusError::Crc => CanError::Crc,
-                        embassy_stm32::can::enums::BusError::Software => CanError::Software,
-                        embassy_stm32::can::enums::BusError::BusOff => CanError::BusOff,
-                        embassy_stm32::can::enums::BusError::BusPassive => CanError::BusPassive,
-                        embassy_stm32::can::enums::BusError::BusWarning => CanError::BusWarning,
+                        embassy_stm32::can::enums::FrameCreateError::NotEnoughData => {
+                            CanError::NotEnoughData
+                        }
+                        embassy_stm32::can::enums::FrameCreateError::InvalidDataLength => {
+                            CanError::InvalidDataLength
+                        }
+                        embassy_stm32::can::enums::FrameCreateError::InvalidCanId => CanError::InvalidCanId,
                     }),
                 }
             }
         }
+
 
         impl #impl_generics #name #ty_generics {
             pub fn new(can: &'static Mutex<NoopRawMutex, RefCell<Can<'static, Blocking>>>) -> Self {
--- lib/io/hyped_can/src/lib.rs
+++ lib/io/hyped_can/src/lib.rs
@@ -3,7 +3,8 @@
 /// CAN errors that can occur
 /// From: https://docs.embassy.dev/embassy-stm32/git/stm32f767zi/can/enums/enum.BusError.html,
 /// https://docs.embassy.dev/embassy-stm32/git/stm32f767zi/can/enum.TryWriteError.html
-/// and https://docs.embassy.dev/embassy-stm32/git/stm32f767zi/can/enum.TryReadError.html
+/// https://docs.embassy.dev/embassy-stm32/git/stm32f767zi/can/enum.TryReadError.html,
+/// and https://docs.embassy.dev/embassy-stm32/git/stm32f767zi/can/enums/enum.FrameCreateError.html
 #[derive(Debug)]
 pub enum CanError {
     Stuff,
@@ -19,45 +20,32 @@ pub enum CanError {
     Full,
     Empty,
     Unknown,
+    NotEnoughData,
+    InvalidDataLength,
+    InvalidCanId,
 }
 #[derive(Clone)]
 pub struct CanFrame {
-    can_id: u32,   // 32 bit CAN_ID + EFF/RTR/ERR flags
-    data: [u8; 8], // data that is sent over CAN, split into bytes
+    pub can_id: u32,   // 32 bit CAN_ID + EFF/RTR/ERR flags
+    pub data: [u8; 8], // data that is sent over CAN, split into bytes
 }
 
 pub type Timestamp = embassy_time::Instant;
 
 #[derive(Clone)]
-pub struct Envelope {
+pub struct HypedEnvelope {
     /// Reception time.
     pub ts: Timestamp,
     /// The actual CAN frame.
     pub frame: CanFrame,
 }
 
-impl Envelope {
-    /// Convert into a tuple
-    pub fn parts(self) -> (CanFrame, Timestamp) {
-        (self.frame, self.ts)
-    }
-}
-
 /// CAN trait used to abstract the CAN operations
 pub trait HypedCan {
-    /// Read a CAN frame.
-    ///
-    /// If no CAN frame is in the RX buffer, this will wait until there is one.
-    /// Returns a tuple of the time the message was received and the message frame
-    async fn read_frame(&mut self) -> Result<Envelope, CanError>;
     /// Attempts to read a CAN frame without blocking.
     ///
     /// Returns [Err(TryReadError::Empty)] if there are no frames in the rx queue.
-    fn try_read_frame(&mut self) -> Result<Envelope, CanError>;
-    /// Queues the message to be sent.
-    ///
-    /// If the TX queue is full, this will wait until there is space, therefore exerting backpressure.
-    async fn write_frame(&mut self, frame: &CanFrame);
+    fn read_frame(&mut self) -> Result<HypedEnvelope, CanError>;
     /// Attempts to transmit a frame without blocking.
     ///
     /// Returns [Err(CanError::Full)] if the frame can not be queued for transmission now.
@@ -66,7 +54,7 @@ pub trait HypedCan {
     ///
     /// Otherwise, the frame will only be accepted if there is no frame with the same priority already queued. This is done
     /// to work around a hardware limitation that could lead to out-of-order delivery of frames with the same priority.
-    fn try_write_frame(&mut self, frame: &CanFrame) -> Result<(), CanError>;
+    fn write_frame(&mut self, frame: &CanFrame) -> Result<(), CanError>;
 }
 
 pub mod mock_can {
@@ -81,32 +69,24 @@ pub mod mock_can {
 
     /// A mock CAN instance which can be used for testing
     pub struct MockCan<'a> {
+        /// Values that have been read from the CAN bus
         frames_to_read: &'a Mutex<CriticalSectionRawMutex, RefCell<CanValues>>,
+        /// Values that have been sent over the CAN bus
         frames_sent: CanValues,
-        try_outcomes: bool,
+        /// Whether to fail reading frames
+        fail_read: bool,
+        /// Whether to fail writing frames
+        fail_write: bool,
     }
 
     impl crate::HypedCan for MockCan<'_> {
-        /// Read a frame from the CAN bus
-        async fn read_frame(&mut self) -> Result<super::Envelope, super::CanError> {
-            self.frames_to_read.lock(|frames_to_read| {
-                match frames_to_read.borrow_mut().pop_front() {
-                    Some(frame) => Ok(super::Envelope {
-                        ts: embassy_time::Instant::now(),
-                        frame,
-                    }),
-                    None => Err(super::CanError::Unknown),
-                }
-            })
-        }
-
-        fn try_read_frame(&mut self) -> Result<super::Envelope, super::CanError> {
-            if !self.try_outcomes {
+        fn read_frame(&mut self) -> Result<super::HypedEnvelope, super::CanError> {
+            if self.fail_read {
                 return Err(super::CanError::Unknown);
             }
             self.frames_to_read.lock(|frames_to_read| {
                 match frames_to_read.borrow_mut().pop_front() {
-                    Some(frame) => Ok(super::Envelope {
+                    Some(frame) => Ok(super::HypedEnvelope {
                         ts: embassy_time::Instant::now(),
                         frame,
                     }),
@@ -115,12 +95,8 @@ pub mod mock_can {
             })
         }
 
-        async fn write_frame(&mut self, frame: &super::CanFrame) {
-            self.frames_sent.push_front(frame.clone());
-        }
-
-        fn try_write_frame(&mut self, frame: &super::CanFrame) -> Result<(), super::CanError> {
-            if !self.try_outcomes {
+        fn write_frame(&mut self, frame: &super::CanFrame) -> Result<(), super::CanError> {
+            if self.fail_write {
                 return Err(super::CanError::Unknown);
             }
             match self.frames_sent.push_front(frame.clone()) {
@@ -134,23 +110,41 @@ pub mod mock_can {
         pub fn new(
             frames_to_read: &'static Mutex<CriticalSectionRawMutex, RefCell<CanValues>>,
         ) -> Self {
+            MockCan::new_with_failures(frames_to_read, false, false)
+        }
+
+        pub fn new_with_failures(
+            frames_to_read: &'static Mutex<CriticalSectionRawMutex, RefCell<CanValues>>,
+            fail_read: bool,
+            fail_write: bool,
+        ) -> Self {
             MockCan {
                 frames_to_read,
                 frames_sent: CanValues::new(),
-                try_outcomes: true,
+                fail_read,
+                fail_write,
             }
         }
+
         /// Get the values that have been sent over the CAN bus
         pub fn get_can_frames(&self) -> &CanValues {
             &self.frames_sent
         }
 
-        pub fn set_try_to_fail(&mut self) {
-            self.try_outcomes = false;
+        pub fn set_read_to_fail(&mut self) {
+            self.fail_read = true;
         }
 
-        pub fn set_try_to_pass(&mut self) {
-            self.try_outcomes = true;
+        pub fn set_write_to_fail(&mut self) {
+            self.fail_write = true;
+        }
+
+        pub fn set_read_to_pass(&mut self) {
+            self.fail_read = false;
+        }
+
+        pub fn set_write_to_pass(&mut self) {
+            self.fail_write = false;
         }
     }
 }
