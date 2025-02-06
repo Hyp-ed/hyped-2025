f39d76d24c1b228f6f0a524c64a7f4dcef377751
jpfbastos <jpfbastos05@gmail.com>
2025-02-06 18:17:49 +0000

    ok now yes

--- boards/stm32f767zi/src/io.rs
+++ boards/stm32f767zi/src/io.rs
@@ -1,16 +1,10 @@
 use core::cell::RefCell;
 use embassy_stm32::adc::{Adc, AnyAdcChannel, Instance};
-use embassy_stm32::can::{
-    enums::{BusError, FrameCreateError, TryReadError},
-    frame, Can, ExtendedId, Frame, Id, StandardId, TryWriteError,
-};
 use embassy_stm32::gpio::{Input, Output};
 use embassy_stm32::{i2c::I2c, mode::Blocking};
 use embassy_sync::blocking_mutex::{raw::NoopRawMutex, Mutex};
-
 use hyped_adc::HypedAdc;
 use hyped_adc_derive::HypedAdc;
-use hyped_can::{CanError, HypedCan, HypedCanFrame, HypedEnvelope};
 use hyped_gpio::{HypedGpioInputPin, HypedGpioOutputPin};
 use hyped_gpio_derive::{HypedGpioInputPin, HypedGpioOutputPin};
 use hyped_i2c::{HypedI2c, I2cError};
