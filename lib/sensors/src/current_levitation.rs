9a3ecf2e63b440b1b601ae4296a19ece59636c6d
Aux1r <ThePigZilla@gmail.com>
2025-02-07 13:40:13 +0000

    comment changes + tasks

--- lib/sensors/src/current_levitation.rs
+++ lib/sensors/src/current_levitation.rs
@@ -4,16 +4,7 @@ use hyped_adc::HypedAdc;
 /// current_levitation implements the logic to read current from the ACHS-7121 current sensor using the
 /// Hyped ADC trait.
 ///
-/// The code constantly reads the channel for a current reading. It then processes this reading into the right format
-/// (as seen on the data sheet) and returns this reading for use.
-///
-/// The ACHS-7121 has a current range of +- 10 A, sensitivity of 185 mV/A.
-/// Assuming we're supplying 5V to the sensor, our off-set is 2.5V in the output reading - note that this offset is given
-/// by the supply voltage divided by 2, so if you change the supply voltage, you'll have to change the offset that we subtract
-/// in the processed_current accordingly.
-///
 /// Data sheet: https://docs.broadcom.com/doc/ACHS-712x-DS
-
 pub struct CurrentLevitation<T: HypedAdc> {
     adc: T,
     calculate_bounds: fn(f32) -> SensorValueRange<f32>,
@@ -27,7 +18,10 @@ impl<T: HypedAdc> CurrentLevitation<T> {
             calculate_bounds: default_calculate_bounds,
         }
     }
-    /// Read the voltage from the Vout Pin and process it before returning it - you have to subtract the voltage offset and divide by the sensitivity
+    /// The ACHS-7121 has a current range of +- 10 A, sensitivity of 185 mV/A.
+    /// Assuming we're supplying 5V to the sensor, our off-set is 2.5V in the output reading - note that this offset is given
+    /// by the supply voltage divided by 2, so if you change the supply voltage, you'll have to change the offset that we subtract
+    /// in the read function accordingly.
     pub fn read(&mut self) -> SensorValueRange<f32> {
         let current = self.adc.read_value() as f32;
         (self.calculate_bounds)((current - OFFSET) / SENSITIVITY)
