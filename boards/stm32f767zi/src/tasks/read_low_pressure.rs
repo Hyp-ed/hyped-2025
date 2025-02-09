5d56d5a51eb5d7787485254532290db564f6221f
SinY/CosY <elitan.uk@gmail.com>
2025-02-09 12:19:41 +0000

    changed to #[test], fixed type errors

--- boards/stm32f767zi/src/tasks/read_low_pressure.rs
+++ boards/stm32f767zi/src/tasks/read_low_pressure.rs
@@ -1,15 +1,15 @@
 use crate::io::Stm32f767ziAdc;
-use embassy_stm32::adc::{Adc, AnyAdcChannel, Instance};
+use embassy_stm32::adc::{Adc, AnyAdcChannel};
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Sender};
 use embassy_time::{Duration, Timer};
 use hyped_sensors::low_pressure::LowPressure;
 
 /// Test task that just continually reads pressure from low pressure sensor and prints value to console
-#[embassy_executor::task]
-pub async fn read_low_pressure(
+#[test]
+pub async fn read_low_pressure<T>(
     adc_thing: Adc<'static, T>,
     adc_channel: AnyAdcChannel<T>,
-    sender: Sender<'static, CriticalSectionRawMutex, u32, 1>,
+    sender: Sender<'static, CriticalSectionRawMutex, f32, 1>,
 ) -> ! {
     let mut low_pressure_sensor = LowPressure::new(Stm32f767ziAdc::new(adc_thing, adc_channel));
 
@@ -18,5 +18,3 @@ pub async fn read_low_pressure(
         Timer::after(Duration::from_millis(100)).await;
     }
 }
-
-// replace "T" type later when I understand what is needed
