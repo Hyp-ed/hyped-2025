8024105995c80f3fee6ead6b66c8f8a9ee17fe01
Archie Spiller <83834735+platinumxy@users.noreply.github.com>
2025-02-09 12:32:33 +0000

    forgot to add a file

--- lib/core/src/lib.rs
+++ lib/core/src/lib.rs
@@ -1,8 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+pub mod can_sendable;
 pub mod format_string;
 pub mod log_types;
 pub mod mqtt;
 pub mod mqtt_topics;
 pub mod types;
-pub mod can_sendable;
