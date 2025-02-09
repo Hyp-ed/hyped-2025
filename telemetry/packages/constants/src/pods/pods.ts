b4b7640bf26180d877884a9c26d107cdd0bff92e
Arjun Naha <arjunnaha@users.noreply.github.com>
2025-02-09 13:44:42 +0000

    TEL - Add object types to central config

--- telemetry/packages/constants/src/pods/pods.ts
+++ telemetry/packages/constants/src/pods/pods.ts
@@ -1,19 +1,20 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
-import { PodSchema } from '@hyped/telemetry-types';
-import * as YAML from 'yaml';
-import { z } from 'zod';
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { PodSchema } from "@hyped/telemetry-types";
+import * as YAML from "yaml";
+import { z } from "zod";
+import { measurementTypes } from "./types";
 
-const CONFIG_FILE_NAME = 'pods.yaml';
+const CONFIG_FILE_NAME = "pods.yaml";
 // Root of hyped repo
 const CONFIG_PATH = path.join(
 	__dirname,
-	'..',
-	'..',
-	'..',
-	'..',
-	'..',
-	'config',
+	"..",
+	"..",
+	"..",
+	"..",
+	"..",
+	"config",
 	CONFIG_FILE_NAME,
 );
 
@@ -27,13 +28,38 @@ const RawPodsSchema = z.object({
 	),
 });
 
-const yamlContent = fs.readFileSync(CONFIG_PATH, 'utf8');
+// We also want to check the 'type' field of each measurement and status is one of the object types
+// It would be unwise to add this directly to the schema (in the types package) because it would
+// break the circular dependency between the types and constants packages.
+type MeasurementType = (typeof measurementTypes)[number];
+const validateType = (
+	ctx: z.RefinementCtx,
+	items: Record<string, unknown>,
+	type: "measurements" | "statuses",
+) => {
+	for (const [id, item] of Object.entries(items)) {
+		const itemType = (item as { type: string }).type;
+		if (!measurementTypes.includes(itemType as MeasurementType)) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: `Invalid ${type.slice(0, -1)} type "${itemType}"`,
+				path: [type, id, "type"],
+			});
+		}
+	}
+};
+const ExtendedPodSchema = PodSchema.superRefine((pod, ctx) => {
+	validateType(ctx, pod.measurements, "measurements");
+	validateType(ctx, pod.statuses, "statuses");
+});
+
+const yamlContent = fs.readFileSync(CONFIG_PATH, "utf8");
 const yamlData = RawPodsSchema.parse(YAML.parse(yamlContent));
 
 export const pods = Object.fromEntries(
 	Object.entries(yamlData.pods).map(([podId, podData]) => [
 		podId,
-		PodSchema.parse({
+		ExtendedPodSchema.parse({
 			id: podId,
 			...podData,
 			measurements: Object.fromEntries(
---
+++ telemetry/packages/constants/src/pods/types.ts
@@ -0,0 +1,14 @@
+export const measurementTypes = [
+	"temperature",
+	"acceleration",
+	"pressure",
+	"hall_effect",
+	"displacement",
+	"velocity",
+	"status",
+	"magnetism",
+	"keyence",
+	"resistance",
+	"levitation",
+	"binary-status",
+] as const;
