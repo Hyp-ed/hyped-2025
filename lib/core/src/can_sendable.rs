57f33697fbc80108ba875acf8b8db48c190d5ce6
Archie Spiller <83834735+platinumxy@users.noreply.github.com>
2025-02-09 12:30:18 +0000

    Ready for review

--- lib/core/src/can_sendable.rs
+++ lib/core/src/can_sendable.rs
@@ -1,9 +1,39 @@
 use defmt::error;
 
 pub const CAN_MSG_TYPE_BOOL: u8 = 0;
-pub const CAN_MSG_TYPE_F32 : u8 = 1;
-pub const CAN_MSG_TYPE_U32 : u8 = 2;
-pub const CAN_MSG_TYPE_POSDELTA : u8 = 3;
+pub const CAN_MSG_TYPE_F32: u8 = 1;
+pub const CAN_MSG_TYPE_U32: u8 = 2;
+pub const CAN_MSG_TYPE_POSDELTA: u8 = 3;
+
+/// Checks if a CAN message is a valid message
+pub fn is_valid_can_msg(msg: &[u8; 8]) -> bool {
+    // we assume that board id is valid as ATM we dont know how many boards sending messages there are
+    (msg[0] & 0x0F) < 4
+}
+
+/// Extracts the message type from a CAN message
+pub fn can_msg_type_from_u8(msg: &[u8; 8]) -> CanMsgType {
+    match msg[0] & 0x0F {
+        CAN_MSG_TYPE_BOOL => CanMsgType::Bool,
+        CAN_MSG_TYPE_F32 => CanMsgType::F32,
+        CAN_MSG_TYPE_U32 => CanMsgType::U32,
+        CAN_MSG_TYPE_POSDELTA => CanMsgType::PosDelta,
+        _ => {
+            error!("Unknown CAN message type: {}", msg[0] & 0x0F);
+            panic!();
+        }
+    }
+}
+
+/// Extracts the board id from a CAN message
+pub fn get_board_id(msg: &[u8; 8]) -> u8 {
+    msg[0] >> 4
+}
+
+/// Builds a CAN header byte from a board id and message type
+pub fn build_can_header(board_id: u8, msg_type: u8) -> u8 {
+    (board_id << 4) | msg_type
+}
 
 pub enum CanMsgType {
     Bool,
@@ -12,35 +42,16 @@ pub enum CanMsgType {
     PosDelta,
 }
 
-pub fn can_msg_type_from_u8(msg_type: &[u8; 8]) -> CanMsgType {
-    match msg_type[0] & 0x0F {
-        CAN_MSG_TYPE_BOOL => CanMsgType::Bool,
-        CAN_MSG_TYPE_F32 => CanMsgType::F32,
-        CAN_MSG_TYPE_U32 => CanMsgType::U32,
-        CAN_MSG_TYPE_POSDELTA => CanMsgType::PosDelta,
-        _ => {
-            error!("Unknown CAN message type: {}", msg_type[0] & 0x0F);
-            panic!();
-        },
-    }
-}
-
-pub trait CanSendable { 
-    fn encode_to_can(&self, board_id:u8) -> [u8; 8];
+pub trait CanSendable {
+    fn encode_to_can(&self, board_id: u8) -> [u8; 8];
     fn can_decode(can_msg: &[u8; 8]) -> Self;
 }
 
-pub fn build_can_header(board_id:u8, msg_type:u8) -> u8 {
-    // 4 bits for each of board_id, msg_type
-    (board_id << 4) | msg_type
-
-}
-    
 impl CanSendable for bool {
-    fn encode_to_can(&self, board_id:u8) -> [u8; 8] {
-        let mut data: [u8;8] = [0;8];
+    fn encode_to_can(&self, board_id: u8) -> [u8; 8] {
+        let mut data: [u8; 8] = [0; 8];
         data[0] = build_can_header(board_id, CAN_MSG_TYPE_BOOL);
-        data[1] = *self as u8; 
+        data[1] = *self as u8;
         return data;
     }
 
@@ -50,8 +61,8 @@ impl CanSendable for bool {
 }
 
 impl CanSendable for [u16; 2] {
-    fn encode_to_can(&self, board_id:u8) -> [u8; 8] {
-        let mut data: [u8;8] = [0;8];
+    fn encode_to_can(&self, board_id: u8) -> [u8; 8] {
+        let mut data: [u8; 8] = [0; 8];
         data[0] = build_can_header(board_id, CAN_MSG_TYPE_U32);
 
         let u16_bytes: [u8; 2] = self[0].to_le_bytes();
@@ -62,7 +73,7 @@ impl CanSendable for [u16; 2] {
 
         return data;
     }
-    
+
     fn can_decode(can_msg: &[u8; 8]) -> Self {
         let mut u16_bytes: [u8; 2] = [0; 2];
         u16_bytes.copy_from_slice(&can_msg[1..3]);
@@ -76,15 +87,15 @@ impl CanSendable for [u16; 2] {
 }
 
 impl CanSendable for f32 {
-    fn encode_to_can(&self, board_id:u8) -> [u8; 8] {
-        let mut data: [u8;8] = [0;8];
+    fn encode_to_can(&self, board_id: u8) -> [u8; 8] {
+        let mut data: [u8; 8] = [0; 8];
         data[0] = build_can_header(board_id, CAN_MSG_TYPE_F32);
 
         let f32_bytes: [u8; 4] = self.to_le_bytes();
         data[1..5].copy_from_slice(&f32_bytes);
         return data;
     }
-    
+
     fn can_decode(can_msg: &[u8; 8]) -> Self {
         let mut f32_bytes: [u8; 4] = [0; 4];
         f32_bytes.copy_from_slice(&can_msg[1..5]);
@@ -92,15 +103,17 @@ impl CanSendable for f32 {
     }
 }
 
-pub struct PositionDelta { // !!!TODO!!! add support for ensuring were decoding only the correct board
+// Struct to hold f32 so they can be sent and decoded over CAN
+pub struct PositionDelta {
     pub clock: Option<u8>,
     pub x: Option<f32>,
     pub y: Option<f32>,
     pub z: Option<f32>,
 }
 
-impl PositionDelta { // sender side code
-    pub fn new(clock:u8, x: f32, y: f32, z: f32) -> Self {
+impl PositionDelta {
+    // sender side code
+    pub fn new(clock: u8, x: f32, y: f32, z: f32) -> Self {
         PositionDelta {
             clock: Some(clock),
             x: Some(x),
@@ -108,19 +121,22 @@ impl PositionDelta { // sender side code
             z: Some(z),
         }
     }
-    
-    // converts a provied F32 
-    pub fn encode_to_can(&self, board_id:u8) -> [[u8; 8]; 3] {
-        assert!(self.is_complete(), "Attempted to encode an incomplete PositionDelta");
+
+    // converts a provied F32
+    pub fn encode_to_can(&self, board_id: u8) -> [[u8; 8]; 3] {
+        assert!(
+            self.is_complete(),
+            "Attempted to encode an incomplete PositionDelta"
+        );
 
         let mut x = self.x.unwrap().encode_to_can(board_id);
-        let mut y = self.y.unwrap().encode_to_can(board_id);        
+        let mut y = self.y.unwrap().encode_to_can(board_id);
         let mut z = self.z.unwrap().encode_to_can(board_id);
 
         x[0] = build_can_header(board_id, CAN_MSG_TYPE_POSDELTA);
         y[0] = build_can_header(board_id, CAN_MSG_TYPE_POSDELTA);
         z[0] = build_can_header(board_id, CAN_MSG_TYPE_POSDELTA);
-        
+
         x[5] = 0;
         y[5] = 1;
         z[5] = 2;
@@ -133,11 +149,11 @@ impl PositionDelta { // sender side code
     }
 }
 
-
-impl PositionDelta { // reciver code 
+impl PositionDelta {
+    // reciver code
 
     /// Create empty PositionDelta to fill
-    pub fn create_new() -> Self {
+    pub fn new_empty() -> Self {
         PositionDelta {
             clock: None,
             x: None,
@@ -150,7 +166,7 @@ impl PositionDelta { // reciver code
     pub fn clear(&mut self) {
         self.clock = None;
         self.x = None;
-        self.y = None; 
+        self.y = None;
         self.z = None;
     }
 
@@ -161,24 +177,25 @@ impl PositionDelta { // reciver code
 
     /// Returns finished f32
     pub fn return_complete(&self) -> [f32; 3] {
-        assert!(self.is_complete(), "Attempted to finalise an incomplete PositionDelta");
+        assert!(
+            self.is_complete(),
+            "Attempted to finalise an incomplete PositionDelta"
+        );
         [self.x.unwrap(), self.y.unwrap(), self.z.unwrap()]
     }
 
-
     /// Atempt to decode a CAN PositionDelta value and add it to the current struture
-    /// example implementation 
+    /// example implementation
     /// ```rust
-    /// let mut pos_d = PositionDelta::new();
+    /// use hyped_core::can_sendable::*;
+    ///
+    /// let mut pos_d = PositionDelta::new_empty();
     /// let mut err_cnt = 0;
-    /// while (running()){
-    ///     let next_val = get_next_pos_delta();
+    /// loop {
+    ///     let next_val = [0; 8]; // get next CAN msg
     ///     if let Some(err) = pos_d.can_decode_step(next_val){
     ///         match err {
-    ///             PositionDeltaDecodeError::PositionAlreadyReceived => { 
-    ///                 error!("Got duplicate CAN msg"); 
-    ///                 panic!();    
-    ///             }
+    ///             PositionDeltaDecodeError::PositionAlreadyReceived => { /* ... throw error and panic */ }
     ///             PositionDeltaDecodeError::ClockOutdated => { /* we dont need to do anything*/ }
     ///             PositionDeltaDecodeError::ClockInFuture => {
     ///                 err_cnt += 1;
@@ -187,54 +204,67 @@ impl PositionDelta { // reciver code
     ///             }
     ///         }
     ///     }
-    ///     if (err_cnt > 3) { 
+    ///     if (err_cnt > 3) {
     ///         // ... emergency exit    
     ///     }
     ///     
     ///     if (pos_d.is_complete()){
-    ///         let err_cnt = 0; 
-    ///         let vals = return_complete();
+    ///         let err_cnt = 0;
+    ///         let vals = pos_d.return_complete();
     ///         // ... do somthing with complete values
     ///     }
     ///     
     /// }
     /// ```
-    pub fn can_decode_step(&mut self, step:[u8; 8]) -> Option<PositionDeltaDecodeError> {
+    pub fn can_decode_step(&mut self, step: [u8; 8]) -> Option<PositionDeltaDecodeError> {
         let step_type = step[5];
         let step_clock = step[6];
 
         if self.clock.is_none() {
             self.clock = Some(step_clock);
         } else if self.clock.unwrap() != step_clock {
-            return Some(if Self::is_clock_infuture(self.clock.unwrap(), step_clock) {
-                PositionDeltaDecodeError::ClockInFuture
-            } else {
-                PositionDeltaDecodeError::ClockOutdated
-            });
+            return Some(
+                if Self::is_clock_infuture(self.clock.unwrap(), step_clock) {
+                    PositionDeltaDecodeError::ClockInFuture
+                } else {
+                    PositionDeltaDecodeError::ClockOutdated
+                },
+            );
         }
 
         let step_data = f32::can_decode(&step);
         match step_type {
             0 => {
-                if self.x.is_none() { self.x = Some(step_data); } 
-                else { return Some(PositionDeltaDecodeError::PositionAlreadyReceived); }
-            }, 1 => {
-                if self.y.is_none() { self.y = Some(step_data); } 
-                else { return Some(PositionDeltaDecodeError::PositionAlreadyReceived); }
-            }, 2 => {
-                if self.z.is_none() { self.z = Some(step_data); } 
-                else { return Some(PositionDeltaDecodeError::PositionAlreadyReceived); }
-            }, _ => {
+                if self.x.is_none() {
+                    self.x = Some(step_data);
+                } else {
+                    return Some(PositionDeltaDecodeError::PositionAlreadyReceived);
+                }
+            }
+            1 => {
+                if self.y.is_none() {
+                    self.y = Some(step_data);
+                } else {
+                    return Some(PositionDeltaDecodeError::PositionAlreadyReceived);
+                }
+            }
+            2 => {
+                if self.z.is_none() {
+                    self.z = Some(step_data);
+                } else {
+                    return Some(PositionDeltaDecodeError::PositionAlreadyReceived);
+                }
+            }
+            _ => {
                 error!("Unknown PositionDelta step type: {}", step_type);
                 panic!();
             }
         }
         None
-        
     }
 
     /// Checks if new clock is infront of the old
-    pub fn is_clock_infuture(old: u8, new: u8 ) -> bool {
+    pub fn is_clock_infuture(old: u8, new: u8) -> bool {
         let diff = new.wrapping_sub(old);
         diff < 128 // assume any diff > 128 is a wraparound
     }
@@ -243,22 +273,21 @@ impl PositionDelta { // reciver code
     pub fn next_clock(old: u8) -> u8 {
         old.wrapping_add(1)
     }
-
 }
 
 /// All the possible failure reasons for failing to decode a position delta step
 pub enum PositionDeltaDecodeError {
     /// The x y or z value for a given clock cycle has already been recived.
-    /// This error shouldn't ever be seen in normal operation, if it is that 
-    /// means that there is a **major issue** with the execution flow, either the 
-    /// CAN sender of the board is sending repeats or were are somehow an 
+    /// This error shouldn't ever be seen in normal operation, if it is that
+    /// means that there is a **major issue** with the execution flow, either the
+    /// CAN sender of the board is sending repeats or were are somehow an
     /// entire u8 out of sync (should be reason to stop the pod)
     PositionAlreadyReceived,
-    /// The clock value we've recived is in the future and so the current 
-    /// cycle should be discarded. The no. of discarded cycles should be 
+    /// The clock value we've recived is in the future and so the current
+    /// cycle should be discarded. The no. of discarded cycles should be
     /// recorded and if it reaches a given threashold (recomended 3~5) the
     /// pod should come to a stop as its current readings are too out of date
     ClockInFuture,
-    /// Clock cycle of an old clock cycle has been recived 
-    ClockOutdated
+    /// Clock cycle of an old clock cycle has been recived
+    ClockOutdated,
 }
