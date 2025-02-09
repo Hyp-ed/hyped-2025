0a67808ff729a245eb5aad87e1bbe4912c4b671a
Michael Belyaev <michael7belyaev@gmail.com>
2025-02-09 17:00:07 +0000

    addded second stripe counter

--- boards/stm32f767zi/src/bin/loc.rs
+++ boards/stm32f767zi/src/bin/loc.rs
@@ -20,7 +20,8 @@ use hyped_localisation::{
 };
 
 /// A Watch to hold the latest Keyence stripe count.
-static KEYENCE_STRIPE_COUNT: Watch<CriticalSectionRawMutex, u32, 1> = Watch::new();
+static KEYENCE_1_STRIPE_COUNT: Watch<CriticalSectionRawMutex, u32, 1> = Watch::new();
+static KEYENCE_2_STRIPE_COUNT: Watch<CriticalSectionRawMutex, u32, 1> = Watch::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) -> ! {
@@ -29,10 +30,13 @@ async fn main(spawner: Spawner) -> ! {
     let gpio_pin = Input::new(p.PC13, Pull::Down);
 
     // Create a sender and a receiver for the Keyence stripe count.
-    let sender = KEYENCE_STRIPE_COUNT.sender();
-    let mut receiver = KEYENCE_STRIPE_COUNT.receiver().unwrap();
+    let sender1 = KEYENCE_1_STRIPE_COUNT.sender();
+    let mut receiver1 = KEYENCE_1_STRIPE_COUNT.receiver().unwrap();
+    let sender2 = KEYENCE_2_STRIPE_COUNT.sender();
+    let mut receiver2 = KEYENCE_2_STRIPE_COUNT.receiver().unwrap();
 
-    spawner.spawn(read_keyence(gpio_pin, sender)).unwrap();
+    spawner.spawn(read_keyence(gpio_pin, sender1)).unwrap();
+    spawner.spawn(read_keyence(gpio_pin, sender2)).unwrap();
 
     info!("Starting localizer loop...");
 
@@ -40,13 +44,16 @@ async fn main(spawner: Spawner) -> ! {
 
     loop {
         // Wait for a new Keyence stripe count.
-        let new_stripe_count = receiver.get().await;
-        defmt::info!("New Keyence stripe count: {}", new_stripe_count);
+        let (stripe_count1, stripe_count2) = join!(receiver1.get(), receiver2.get());
+        defmt::info!(
+            "New Keyence stripe counts: sensor1 = {}, sensor2 = {}",
+            stripe_count1,
+            stripe_count2
+        );
 
         // Create the sensor data. (Optical and accelerometer data are simulated.)
         let optical_data: Vec<f64, 2> = Vec::from_slice(&[0.5, 0.5]).unwrap();
-        let keyence_data: Vec<u32, 2> =
-            Vec::from_slice(&[new_stripe_count, new_stripe_count]).unwrap();
+        let keyence_data: Vec<u32, 2> = Vec::from_slice(&[stripe_count1, stripe_count2]).unwrap();
         let accelerometer_data: RawAccelerometerData<NUM_ACCELEROMETERS, NUM_AXIS> =
             RawAccelerometerData::from_slice(&[
                 Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
