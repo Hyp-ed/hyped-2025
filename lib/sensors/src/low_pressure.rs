c7e127183bfdf5ff522bd1f4c2947746eb34108e
SinY/CosY <elitan.uk@gmail.com>
2025-02-09 10:40:41 +0000

    moved GRADIENT_LOW to global constant, removed comments

--- lib/sensors/src/low_pressure.rs
+++ lib/sensors/src/low_pressure.rs
@@ -3,7 +3,13 @@ use hyped_adc::HypedAdc;
 /// The low pressure sensor (LPS) (model: SPAN-P10R-G18F-PNLK-PNVBA-L1) is able to detect
 /// pressure in range from 0 to 10 bar. The sensor utilises the ADC protocol to get the
 /// pressure value, and its conversion rate is expressed as a linear function of:
-///  
+///     pressure = (conversion gradient) * (ADC reading) + (minimum pressure value)
+///     (y = mx + c0)
+/// where conversion gradient is
+///     (maximum pressure value + minimum pressure value) / (4096).
+/// 4096 is the maximum ADC reading value.
+/// Since LPS has a minimum pressure of 0 bar, c0 is 0 and was did not need to be included in
+/// the source code.
 
 /// Links to datasheets
 ///     (https://www.festo.com/gb/en/a/download-document/datasheet/8134897)
@@ -12,6 +18,8 @@ use hyped_adc::HypedAdc;
 pub struct LowPressure<T: HypedAdc> {
     adc: T
 }
+
+const GRADIENT_LOW: f32 = 0.00244;
 
 impl<T: HypedAdc> LowPressure<T> {
     /// Create new low pressure sensor instance
@@ -23,10 +31,6 @@ impl<T: HypedAdc> LowPressure<T> {
 
     /// Convert ADC reading to bar unit and assign to pressure variable
     pub fn read_pressure(&mut self) -> u16 {
-        // conversion gradient value
-        const GRADIENT_LOW: f32 = 0.00244;
-
-        // read ADC
         let adc_val = self.adc.read_value();
         
         // convert to bar unit
@@ -35,9 +39,4 @@ impl<T: HypedAdc> LowPressure<T> {
 
         bar_pressure_val
     }
-}
-
-#[cfg(test)]
-mod tests {
-    
 }
