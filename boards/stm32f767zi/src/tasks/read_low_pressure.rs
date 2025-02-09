af46e82c204f5a462f6e7c751e8abd8fc383f7f5
SinY/CosY <elitan.uk@gmail.com>
2025-02-09 11:52:04 +0000

    implemented task

---
+++ boards/stm32f767zi/src/tasks/read_low_pressure.rs
@@ -0,0 +1,21 @@
+use crate::io::Stm32f767ziAdc;
+use embassy_time::{Duration, Timer};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Sender};
+use hyped_sensors::low_pressure::LowPressure;
+
+/// Test task that just continually reads pressure from low pressure sensor and prints value to console
+#[embassy_executor::task]
+pub async fn read_low_pressure(
+    adc_pin: Adc<'d, T>,
+    adc_channel: AnyAdcChannel<T>,
+    sender: Sender<'static, CriticalSectionRawMutex, u32, 1>,
+) -> ! {
+    let mut low_pressre_sensor = LowPressure::new(Stm32f767ziAdc::new(adc_pin, adc_channel));
+
+    loop {
+        Sender.send(low_pressure_sensor.read_pressure());
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
+
+// replace "T" type later when I understand what is needed
