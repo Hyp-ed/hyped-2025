0b3c38ec2b1a01ac5ed60f601ee620a0c1aec273
SinY/CosY <elitan.uk@gmail.com>
2025-02-09 13:25:48 +0000

    replaced sender with defmt info macro

--- boards/stm32f767zi/src/bin/read_low_pressure.rs
+++ boards/stm32f767zi/src/bin/read_low_pressure.rs
@@ -4,7 +4,6 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_stm32::adc::{Adc, AdcChannel};
-use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Sender};
 use embassy_time::{Duration, Timer};
 use hyped_boards_stm32f767zi::io::Stm32f767ziAdc;
 use hyped_sensors::low_pressure::LowPressure;
@@ -13,16 +12,15 @@ use hyped_sensors::low_pressure::LowPressure;
 #[embassy_executor::main]
 pub async fn main(_spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
+    info!("Low pressure sensor");
 
     let mut adc = Adc::new(p.ADC1);
     let pin = p.PA3;
 
-    let sender: Sender<'static, CriticalSectionRawMutex, f32, 1>;
-
     let mut low_pressure_sensor = LowPressure::new(Stm32f767ziAdc::new(adc, pin.degrade_adc()));
 
     loop {
-        sender.send(low_pressure_sensor.read_pressure());
+        info!("{}", low_pressure_sensor.read_pressure());
         Timer::after(Duration::from_millis(100)).await;
     }
 }
