9909c645458421ddd507c88362d47580cb9c101f
jpfbastos <jpfbastos05@gmail.com>
2025-02-09 12:14:49 +0000

    initial prototype for canOpen

---
+++ lib/motors/src/can_processor.rs
@@ -0,0 +1,68 @@
+use crate::can_open_message::{config_messages, messages, CanOpenMessage};
+use hyped_can::{CanError, HypedCan, HypedCanFrame};
+
+pub enum MessagesEnum {
+    TestStepperEnable,
+    TestModeCommand,
+    EnterStopState,
+    EnterPreoperationalState,
+    EnterOperationalState,
+    SetFrequency,
+    Shutdown,
+    SwitchOn,
+    StartDrive,
+    QuickStop,
+}
+
+impl From<CanOpenMessage> for HypedCanFrame {
+    fn from(msg: CanOpenMessage) -> Self {
+        let mut data: [u8; 8] = [0; 8];
+
+        data[0] = msg.command;
+        data[1] = (msg.index & 0xFF) as u8;
+        data[2] = ((msg.index >> 8) & 0xFF) as u8;
+        data[3] = msg.sub_index;
+        data[4] = (msg.data & 0xFF) as u8;
+        data[5] = ((msg.data >> 8) & 0xFF) as u8;
+        data[6] = ((msg.data >> 16) & 0xFF) as u8;
+        data[7] = ((msg.data >> 24) & 0xFF) as u8;
+
+        let frame = HypedCanFrame {
+            can_id: msg.id,
+            data,
+        };
+        frame
+    }
+}
+
+impl From<MessagesEnum> for CanOpenMessage {
+    fn from(message: MessagesEnum) -> Self {
+        match message {
+            MessagesEnum::TestStepperEnable => config_messages::TEST_STEPPER_ENABLE,
+            MessagesEnum::TestModeCommand => config_messages::TEST_MODE_COMMAND,
+            MessagesEnum::EnterStopState => messages::ENTER_STOP_STATE,
+            MessagesEnum::EnterPreoperationalState => messages::ENTER_PREOPERATIONAL_STATE,
+            MessagesEnum::EnterOperationalState => messages::ENTER_OPERATIONAL_STATE,
+            MessagesEnum::SetFrequency => messages::SET_FREQUENCY,
+            MessagesEnum::Shutdown => messages::SHUTDOWN,
+            MessagesEnum::SwitchOn => messages::SWITCH_ON,
+            MessagesEnum::StartDrive => messages::START_DRIVE,
+            MessagesEnum::QuickStop => messages::QUICK_STOP,
+        }
+    }
+}
+
+pub struct CanOpen<T: HypedCan> {
+    can: T,
+}
+
+impl<T: HypedCan> CanOpen<T> {
+    pub fn new(can: T) -> Self {
+        CanOpen { can }
+    }
+
+    pub fn send_message(&mut self, message: MessagesEnum) -> Result<(), CanError> {
+        let frame: HypedCanFrame = CanOpenMessage::from(message).into();
+        self.can.write_frame(&frame)
+    }
+}
---
+++ lib/motors/src/lib.rs
@@ -0,0 +1,4 @@
+#![no_std]
+
+pub mod can_open_message;
+pub mod can_processor;
