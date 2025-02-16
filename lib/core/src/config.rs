8d278e89d992ea5f3b92992f882cfc14437b037a
Tom Lonergan <tomlonergan91@gmail.com>
2025-02-16 23:25:33 +0000

    config parsing using my new crate !!!!

---
+++ lib/core/src/config.rs
@@ -0,0 +1,22 @@
+use config_to_rs::config_to_rs;
+
+/// Configuration for the pods
+/// The configuration is loaded from the `config/pods.yaml` file, and can be read using standard
+/// Rust syntax.
+///
+/// E.g. to get the pod name,
+/// ```rust
+/// let pod_name = CONFIG.pods.poddington.label;
+///
+/// assert_eq!(pod_name, "Poddington");
+/// ````
+#[config_to_rs(yaml, "config/pods.yaml")]
+pub struct Config;
+
+mod test {
+    #[test]
+    fn test_config() {
+        let pod_name = super::CONFIG.pods.poddington.label;
+        assert_eq!(pod_name, "Poddington");
+    }
+}
