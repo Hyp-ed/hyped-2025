9c8d91b004d9bf88c3f3e75a928d65c4a4b2bae9
David Beechey <david@dtbeechey.dev>
2025-02-17 15:57:25 +0000

    formatting

--- lib/motors/src/lib.rs
+++ lib/motors/src/lib.rs
@@ -1,8 +1,8 @@
 #![no_std]
 
+pub mod can_open_message;
+pub mod can_open_processor;
 pub mod constant_frequency_calculator;
 pub mod frequency_calculator;
 pub mod time_frequency_calculator;
 pub mod velocity_frequency_calculator;
-pub mod can_open_message;
-pub mod can_open_processor;
