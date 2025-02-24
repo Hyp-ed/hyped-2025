100ab473c3352df73f421d433381e5dcde243cea
SinY/CosY <elitan.uk@gmail.com>
2025-02-24 19:21:32 +0000

    modified to accommodate sensor range value handling

--- lib/sensors/src/low_pressure.rs
+++ lib/sensors/src/low_pressure.rs
@@ -1,5 +1,7 @@
 use hyped_adc::HypedAdc;
 
+use crate::SensorValueRange;
+
 /// The low pressure sensor (LPS) (model: SPAN-P10R-G18F-PNLK-PNVBA-L1) is able to detect
 /// pressure in range from 0 to 10 bar. The sensor utilises the ADC protocol to get the
 /// pressure value.
@@ -9,6 +11,7 @@ use hyped_adc::HypedAdc;
 ///     (https://www.festo.com/media/catalog/203714_documentation.pdf)
 pub struct LowPressure<T: HypedAdc> {
     adc: T,
+    calculate_bounds: fn(f32) -> SensorValueRange<f32>,
 }
 
 const MAX_PRESSURE: f32 = 10.0;
@@ -17,8 +20,19 @@ const GRADIENT_LOW: f32 = MAX_PRESSURE / ADC_RESOLUTION;
 
 impl<T: HypedAdc> LowPressure<T> {
     /// Create new low pressure sensor instance
-    pub fn new(adc: T) -> LowPressure<T> {
-        LowPressure { adc }
+    pub fn new(adc: T) -> LowPressure<T>{
+        Self::new_with_bounds(adc, default_calculate_bounds)
+    }
+
+    /// Create new low pressure sensor instance with specified bounds
+    pub fn new_with_bounds(
+        adc: T,
+        calculate_bounds: fn(f32) -> SensorValueRange<f32>,
+    ) -> LowPressure<T> {
+        LowPressure {
+            adc,
+            calculate_bounds,
+        } 
     }
 
     /// Convert ADC reading to bar unit and return value to caller
@@ -30,12 +44,32 @@ impl<T: HypedAdc> LowPressure<T> {
     /// 4096 is the maximum ADC reading value.
     /// Since LPS has a minimum pressure of 0 bar, c0 is 0 and was did not need to be included in
     /// the source code.
-    pub fn read_pressure(&mut self) -> f32 {
+    /// wrapped in a SensorValueRange enum to indicate if the temperature is safe, warning, or critical.
+    pub fn read_pressure(&mut self) -> SensorValueRange<f32> {
         let adc_val = self.adc.read_value() as f32;
 
         // convert to bar unit
         let bar_pressure_val: f32 = adc_val * GRADIENT_LOW;
 
-        bar_pressure_val
+        (self.calculate_bounds)(
+            bar_pressure_val
+        )
+    }
+}
+
+/// Default calculation of the bounds for the low pressure sensor. The bounds are set to:
+/// - Safe: 4.0 to 6.0 bar
+/// - Warning: 2.0 to 4.0 and 6.0 to 8.0 bar
+/// - Critical: below 2.0 and above 8.0 bar
+/// TODO: CHANGE WHEN NEW INFO GOTTEN, THESE ARE PLACEHOLDER VALS
+pub fn default_calculate_bounds(value: f32) -> SensorValueRange<f32> {
+    if value <= 2.0 || value >= 8.0 {
+        SensorValueRange::Critical(value)
+    }
+    else if value <= 4.0 || value >= 6.0 {
+        SensorValueRange::Warning(value)
+    }
+    else {
+        SensorValueRange::Safe(value)
     }
 }
