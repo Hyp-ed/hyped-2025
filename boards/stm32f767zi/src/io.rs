035a2f6839d40e94c78bc9adf7dd3713c46c069a
David Beechey <david@dtbeechey.dev>
2025-02-27 18:42:59 +0000

    SNS - Split CAN IO into Rx and Tx (#92)

--- boards/stm32f767zi/src/io.rs
+++ boards/stm32f767zi/src/io.rs
@@ -1,16 +1,21 @@
 use core::cell::RefCell;
-use embassy_stm32::adc::{Adc, AnyAdcChannel, Instance};
-use embassy_stm32::can::{
-    enums::{BusError, FrameCreateError, TryReadError},
-    frame, Can, ExtendedId, Frame, Id, StandardId, TryWriteError,
+
+use embassy_stm32::{
+    adc::{Adc, AnyAdcChannel, Instance},
+    can::{
+        enums::{BusError, FrameCreateError, TryReadError},
+        frame, Can, CanRx, CanTx, ExtendedId, Frame, Id, StandardId, TryWriteError,
+    },
+    gpio::{Input, Output},
+    i2c::I2c,
+    mode::Blocking,
 };
-use embassy_stm32::gpio::{Input, Output};
-use embassy_stm32::{i2c::I2c, mode::Blocking};
 use embassy_sync::blocking_mutex::{raw::NoopRawMutex, Mutex};
+
 use hyped_adc::HypedAdc;
 use hyped_adc_derive::HypedAdc;
-use hyped_can::{CanError, HypedCan, HypedCanFrame, HypedEnvelope};
-use hyped_can_derive::HypedCan;
+use hyped_can::{CanError, HypedCan, HypedCanFrame, HypedCanRx, HypedCanTx, HypedEnvelope};
+use hyped_can_derive::{HypedCan, HypedCanRx, HypedCanTx};
 use hyped_gpio::{HypedGpioInputPin, HypedGpioOutputPin};
 use hyped_gpio_derive::{HypedGpioInputPin, HypedGpioOutputPin};
 use hyped_i2c::{HypedI2c, I2cError};
@@ -40,4 +45,14 @@ pub struct Stm32f767ziI2c<'d> {
 #[derive(HypedCan)]
 pub struct Stm32f767ziCan<'d> {
     can: &'d Mutex<NoopRawMutex, RefCell<&'d mut Can<'static>>>,
+}
+
+#[derive(HypedCanRx)]
+pub struct Stm32f767ziCanRx<'d> {
+    can: &'d Mutex<NoopRawMutex, RefCell<&'d mut CanRx<'static>>>,
+}
+
+#[derive(HypedCanTx)]
+pub struct Stm32f767ziCanTx<'d> {
+    can: &'d Mutex<NoopRawMutex, RefCell<&'d mut CanTx<'static>>>,
 }
