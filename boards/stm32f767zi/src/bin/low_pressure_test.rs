e43199d86a4265be5beb353b9a636b212d7c3527
SinY/CosY <elitan.uk@gmail.com>
2025-02-27 19:17:20 +0000

    added reading handling, work in progress

--- boards/stm32f767zi/src/bin/low_pressure_test.rs
+++ boards/stm32f767zi/src/bin/low_pressure_test.rs
@@ -31,9 +31,36 @@ async fn main(_spawner: Spawner) -> ! {
     let adc = Adc::new(p.ADC1);
     let pin = p.PA3;
 
+    // Create a sender to pass to the temperature reading task, and a receiver for reading the values back.
+    // let low_pres_reading_sender = LOW_PRESSURE_READING_READING.sender();
+    // let mut low_pres_reading_receiver = LOW_PRESSURE_READING_READING.receiver().unwrap();
+
     let mut low_pressure_sensor = LowPressure::new(Stm32f767ziAdc::new(adc, pin.degrade_adc()));
 
+    let low_pres_reading = low_pressure_sensor.read_pressure();
+
+    // spawner
+    //     .low_pressure_sensor.read_pressure()
+
+    // I DON'T UNDERSTAND
     loop {
+        match low_pres_reading.try_changed() {
+            Some(reading) => match reading {
+                Some(reading) => match reading {
+                    Safe(temp) => {
+                        defmt::info!("Temperature: {}°C (safe)", temp);
+                    }
+                    Warning(temp) => {
+                        defmt::warn!("Temperature: {}°C (warning)", temp);
+                    }
+                    Critical(temp) => {
+                        defmt::error!("Temperature: {}°C (critical)", temp);
+                    }
+                },
+                None => defmt::warn!("No temperature reading available."),
+            },
+            None => (),
+        }
 
         info!("{}", );
         Timer::after(Duration::from_millis(100)).await;
