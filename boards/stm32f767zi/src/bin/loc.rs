6673a158cd6f3dba08705974bd9cd16744330ceb
Michael Belyaev <michael7belyaev@gmail.com>
2025-03-06 18:41:31 +0000

    board fmt

--- boards/stm32f767zi/src/bin/loc.rs
+++ boards/stm32f767zi/src/bin/loc.rs
@@ -36,12 +36,8 @@ async fn main(spawner: Spawner) -> ! {
     let sender2 = KEYENCE_2_STRIPE_COUNT.sender();
     let mut receiver2 = KEYENCE_2_STRIPE_COUNT.receiver().unwrap();
 
-    spawner
-        .spawn(read_keyence(gpio_pin1, sender1))
-        .unwrap();
-    spawner
-        .spawn(read_keyence(gpio_pin2, sender2))
-        .unwrap();
+    spawner.spawn(read_keyence(gpio_pin1, sender1)).unwrap();
+    spawner.spawn(read_keyence(gpio_pin2, sender2)).unwrap();
 
     info!("Starting localizer loop...");
 
