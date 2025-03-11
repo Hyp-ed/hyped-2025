d250427eb69d64808add40df2e500c8c559eb3bb
siny-over-cosy <elitan.uk@gmail.com>
2025-03-11 00:17:16 +0000

    missing generic type and 2nd AnyAdcChannel argument

--- boards/stm32f767zi/src/tasks/read_low_pressure.rs
+++ boards/stm32f767zi/src/tasks/read_low_pressure.rs
@@ -13,7 +13,7 @@ use embassy_time::{Duration, Timer};
 use hyped_sensors::low_pressure::LowPressure;
 use hyped_sensors::SensorValueRange;
 
-type Adc1Bus = Mutex<NoopRawMutex, RefCell<Adc<'static, T>>>;
+type Adc1Bus = Mutex<NoopRawMutex, RefCell<Adc<'static, T>>>;   // Not sure what T should be replaced with here, omitting it causes errors saying 'missing generic type'
 
 /// The update frequency of the low pressure sensor in Hz
 const UPDATE_FREQUENCY: u64 = 10;
@@ -24,7 +24,8 @@ pub async fn read_low_pressure(
     adc_bus: &'static Adc1Bus,
     sender: Sender<'static, CriticalSectionRawMutex, Option<SensorValueRange<f32>>, 1>,
 ) -> ! {
-    let mut hyped_adc = Stm32f767ziAdc::new(adc_bus);
+    let mut hyped_adc = Stm32f767ziAdc::new(adc_bus);   // says a 2nd argument of type AnyAdcChannel is missing, but i looked at the function src code and it only has 1 argument?
+                                                        // I can't understand what the cargo build is suggesting for me to do.
 
     let mut low_pressure_sensor = LowPressure::new(hyped_adc);
 
