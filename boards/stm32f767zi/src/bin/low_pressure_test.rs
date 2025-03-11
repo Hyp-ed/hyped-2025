565598fa2a4e5965ffa83fc5af355ff47742dcdd
David Beechey <david@dtbeechey.dev>
2025-03-11 10:28:19 +0000

    Fix ADC stuff

--- boards/stm32f767zi/src/bin/low_pressure_test.rs
+++ boards/stm32f767zi/src/bin/low_pressure_test.rs
@@ -1,24 +1,13 @@
 #![no_std]
 #![no_main]
 
-use core::cell::RefCell;
-
 use embassy_executor::Spawner;
-use embassy_stm32::{adc::Adc, time::Hertz};
-use embassy_sync::{
-    blocking_mutex::{
-        raw::{CriticalSectionRawMutex, NoopRawMutex},
-        Mutex,
-    },
-    watch::Watch,
-};
+use embassy_stm32::adc::{Adc, AdcChannel};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Watch};
 use embassy_time::{Duration, Timer};
 use hyped_boards_stm32f767zi::tasks::read_low_pressure::read_low_pressure;
 use hyped_sensors::SensorValueRange::{self, *};
-use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
-
-type Adc1Bus = Mutex<NoopRawMutex, RefCell<Adc<'static, ADC1>>>;
 
 /// Used to keep the latest low pressure sensor value.
 static LOW_PRESSURE_READING: Watch<CriticalSectionRawMutex, Option<SensorValueRange<f32>>, 1> =
@@ -27,19 +16,16 @@ static LOW_PRESSURE_READING: Watch<CriticalSectionRawMutex, Option<SensorValueRa
 #[embassy_executor::main]
 async fn main(spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
-    let mut adc = Adc::new(p.ADC1);
-
-    // Initialize the ADC and store it in a static cell so that it can be accessed from the tasks.
-    static ADC: StaticCell<Adc1Bus> = StaticCell::new();
-    let adc_access = ADC.init(Mutex::new(RefCell::new(adc)));
 
     // Create a sender to pass to the low pressure reading task, and a receiver for reading the values back.
     let low_pressure_reading_sender = LOW_PRESSURE_READING.sender();
     let mut low_pressure_reading_receiver = LOW_PRESSURE_READING.receiver().unwrap();
 
-    spawner
-        .spawn(read_low_pressure(adc_access, low_pressure_reading_sender))
-        .unwrap();
+    // Initialize the ADC peripheral and the pin that the low pressure sensor is connected to.
+    let adc = Adc::new(p.ADC1);
+    let pin = p.PA3.degrade_adc();
+
+    spawner.must_spawn(read_low_pressure(adc, pin, low_pressure_reading_sender));
 
     // Every 100ms we read for the latest value from the low pressure sensor.
     loop {
@@ -60,6 +46,6 @@ async fn main(spawner: Spawner) -> ! {
             },
             None => (),
         }
-        Timer::after(Duration::from_hz(100)).await;
+        Timer::after(Duration::from_millis(100)).await;
     }
 }
--- boards/stm32f767zi/src/tasks/read_low_pressure.rs
+++ boards/stm32f767zi/src/tasks/read_low_pressure.rs
@@ -1,19 +1,13 @@
 use crate::io::Stm32f767ziAdc;
-use core::cell::RefCell;
 use defmt_rtt as _;
-use embassy_stm32::adc::{Adc, AdcChannel};
-use embassy_sync::{
-    blocking_mutex::{
-        raw::{CriticalSectionRawMutex, NoopRawMutex},
-        Mutex,
-    },
-    watch::Sender,
+use embassy_stm32::{
+    adc::{Adc, AnyAdcChannel},
+    peripherals::ADC1,
 };
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Sender};
 use embassy_time::{Duration, Timer};
 use hyped_sensors::low_pressure::LowPressure;
 use hyped_sensors::SensorValueRange;
-
-type Adc1Bus = Mutex<NoopRawMutex, RefCell<Adc<'static, T>>>;   // Not sure what T should be replaced with here, omitting it causes errors saying 'missing generic type'
 
 /// The update frequency of the low pressure sensor in Hz
 const UPDATE_FREQUENCY: u64 = 10;
@@ -21,12 +15,11 @@ const UPDATE_FREQUENCY: u64 = 10;
 /// Test task that just reads the pressure from the low pressure sensor and prints it to the console
 #[embassy_executor::task]
 pub async fn read_low_pressure(
-    adc_bus: &'static Adc1Bus,
+    adc: Adc<'static, ADC1>,
+    pin: AnyAdcChannel<ADC1>,
     sender: Sender<'static, CriticalSectionRawMutex, Option<SensorValueRange<f32>>, 1>,
 ) -> ! {
-    let mut hyped_adc = Stm32f767ziAdc::new(adc_bus);   // says a 2nd argument of type AnyAdcChannel is missing, but i looked at the function src code and it only has 1 argument?
-                                                        // I can't understand what the cargo build is suggesting for me to do.
-
+    let hyped_adc = Stm32f767ziAdc::new(adc, pin);
     let mut low_pressure_sensor = LowPressure::new(hyped_adc);
 
     loop {
