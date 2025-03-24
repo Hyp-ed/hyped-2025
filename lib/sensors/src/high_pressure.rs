7766f367709603b62b9dbabeb6eeb821306687e2
siny-over-cosy <elitan.uk@gmail.com>
2025-03-24 18:32:07 +0000

    implemented state handling

--- lib/sensors/src/high_pressure.rs
+++ lib/sensors/src/high_pressure.rs
@@ -1,4 +1,3 @@
-use hyped_core::types::DigitalSignal;
 use hyped_gpio::HypedGpioInputPin;
 
 /// The high pressure sensor (SPAW-P25R-G12M-2N-M12) is able to detect pressure in range
@@ -11,49 +10,36 @@ use hyped_gpio::HypedGpioInputPin;
 pub struct HighPressure<T: HypedGpioInputPin> {
     sp1_gpio: T,
     sp2_gpio: T,
-    calculate_bounds: fn(f32) -> SensorValueRange<f32>,
 }
 impl<T: HypedGpioInputPin> HighPressure<T> {
-    /// Create new high pressure sensor instance
+    /// Create new low pressure sensor instance
     pub fn new(
         sp1_gpio: T,
         sp2_gpio: T,
     ) -> HighPressure<T> {
-        Self::new_with_bounds(sp1_gpio, sp2_gpio, default_calculate_bounds)
-    }
-
-    /// Create new low pressure sensor instance with specified bounds 
-    pub fn new_with_bounds(
-        sp1_gpio: T,
-        sp2_gpio: T,
-        calculate_bounds: fn(f32) -> SensorValueRange<f32>,
-    ) -> HighPressure<T> {
         HighPressure {
             sp1_gpio,
             sp2_gpio,
-            calculate_bounds,
         }
     }
 
-    /// Read SP1 and SP2 GPIO pin values and bit OR them. Return state of high pressure sensor based on value of OR'd value.
+    /// Read SP1 and SP2 GPIO pin values and bitwise OR them. Return state of high pressure sensor based on value of OR'd value.
     pub fn get_high_pressure_state(&mut self) -> State {
-        // get pressure from BOTHHH gpio pins
-        // return enum variant corresponding to which 3 states its in
-        // states are affected by which pins are past their thresholds
         let sp1 = self.sp1_gpio.is_high() as u8;
         let sp2 = (self.sp2_gpio.is_high() as u8) << 1;
 
         let pres_state = sp1 | sp2;
 
         match pres_state {
-            0 => State::Off,
-            1 => State::State2,
-            3 => State::State3,
+            0 => State::Off,    // 00 - none are high
+            1 => State::State2, // 01 - sp1 is high
+            3 => State::State3, // 11 - sp1 and sp2 are high
+            _ => State::Off,    // any other case - should never be possible
         }
     }
 }
 
-// offset for pressure value (bar) read from sensor
+// Offset for pressure value (bar) read from sensor
 const PRESSURE_OFFSET: f32 = 0.0;
 
 const MAX_PRESSURE: f32 = 25.0;
