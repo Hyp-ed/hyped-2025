478258bbd84c06ea41ef3384770eac4dba4b3a19
siny-over-cosy <elitan.uk@gmail.com>
2025-03-24 18:50:03 +0000

    added error handling in state function

--- lib/sensors/src/high_pressure.rs
+++ lib/sensors/src/high_pressure.rs
@@ -1,7 +1,11 @@
 use hyped_gpio::HypedGpioInputPin;
 
 /// The high pressure sensor (SPAW-P25R-G12M-2N-M12) is able to detect pressure in range
-/// from 0 to 25 bar.
+/// from 0 to 25 bar. The high pressure sensor has 2 switching outputs, SP1 and SP2. SP2
+/// can only be high when SP1 is high, meaning that the high pressure sensor has 3 states:
+///     Off: Both SP1 and SP2 are LOW
+///     2. SP1 is HIGH and SP2 is LOW
+///     3. Both SP1 and SP2 are HIGH
 /// 
 /// Links to datasheets
 ///     (https://www.festo.com/media/catalog/203715_documentation.pdf)
@@ -24,17 +28,17 @@ impl<T: HypedGpioInputPin> HighPressure<T> {
     }
 
     /// Read SP1 and SP2 GPIO pin values and bitwise OR them. Return state of high pressure sensor based on value of OR'd value.
-    pub fn get_high_pressure_state(&mut self) -> State {
+    pub fn get_high_pressure_state(&mut self) -> Result<State, &'static str> {
         let sp1 = self.sp1_gpio.is_high() as u8;
         let sp2 = (self.sp2_gpio.is_high() as u8) << 1;
 
         let pres_state = sp1 | sp2;
 
         match pres_state {
-            0 => State::Off,    // 00 - none are high
-            1 => State::State2, // 01 - sp1 is high
-            3 => State::State3, // 11 - sp1 and sp2 are high
-            _ => State::Off,    // any other case - should never be possible
+            0 => Ok(State::Off),                        // 00 - none are high
+            1 => Ok(State::State2),                     // 01 - sp1 is high
+            3 => Ok(State::State3),                     // 11 - sp1 and sp2 are high
+            _ => Err("ERROR?? Value: {pres_state}."),   // any other case - should never be possible
         }
     }
 }
@@ -44,4 +48,9 @@ pub enum State {
     Off,
     State2,
     State3
+}
+
+#[cfg(test)]
+mod tests {
+
 }
