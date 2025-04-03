0e1e454e6c222446e45d6d1c9ebaf67ccedf3830
siny-over-cosy <elitan.uk@gmail.com>
2025-04-03 19:03:46 +0100

    added read board task

---
+++ boards/stm32f767zi/src/tasks/read_high_pressure.rs
@@ -0,0 +1,20 @@
+use crate::io::Stm32f767ziGpioInput;
+use embassy_stm32::gpio::Input;
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Sender};
+use embassy_time::{Duration, Timer};
+use hyped_sensors::high_pressure::HighPressure;
+
+#[embassy_executor::task]
+pub async fn read_high_pressure(
+    sp1_pin: Input<'static>,
+    sp2_pin: Input<'static>,
+    sender: Sender<'static, CriticalSectionRawMutex, u32, 1>,
+) -> ! {
+    let mut high_pressure_sensor = HIghPressure::new(Stm32f767ziGpioInput::new(sp1_pin), Stm32f767ziGpioInput::new(sp2_pin));
+
+    loop {
+        high_pressure_sensor.get_high_pressure_state();
+        sender.send(keyence.get_stripe_count());
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
