03a6911a88a817cc9f58b5c2c859b5c6d6234136
siny-over-cosy <elitan.uk@gmail.com>
2025-05-11 17:13:37 +0100

    modified types to hold HighPressureError

--- boards/stm32f767zi/src/tasks/read_high_pressure.rs
+++ boards/stm32f767zi/src/tasks/read_high_pressure.rs
@@ -2,13 +2,13 @@ use crate::io::Stm32f767ziGpioInput;
 use embassy_stm32::gpio::Input;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, watch::Sender};
 use embassy_time::{Duration, Timer};
-use hyped_sensors::high_pressure::{HighPressure, State};
+use hyped_sensors::high_pressure::{HighPressure, HighPressureError, State};
 
 #[embassy_executor::task]
 pub async fn read_high_pressure(
     sp1_pin: Input<'static>,
     sp2_pin: Input<'static>,
-    sender: Sender<'static, CriticalSectionRawMutex, Result<State, &'static str>, 1>,
+    sender: Sender<'static, CriticalSectionRawMutex, Result<State, HighPressureError>, 1>,
 ) -> ! {
     let mut high_pressure_sensor = HighPressure::new(
         Stm32f767ziGpioInput::new(sp1_pin),
