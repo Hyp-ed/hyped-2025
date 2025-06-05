790869cec02072262c80b7d8475ad1f09ede1a55
miksquared <mbagi2004@gmail.com>
2025-06-05 14:50:19 +0100

    mux code - accept vector of mux address/channel pairs

--- boards/stm32f767zi/src/bin/accelerometer_mux_text.rs
+++ boards/stm32f767zi/src/bin/accelerometer_mux_text.rs
@@ -14,24 +14,29 @@ use embassy_sync::{
     watch::Watch,
 };
 use embassy_time::{Duration, Timer};
+use heapless::Vec;
 use hyped_boards_stm32f767zi::tasks::sensors::read_accelerometer_from_mux::{
-    read_accelerometer_from_mux, AccelerometerMuxReadings,
+    read_accelerometer_from_mux, AccelerometerMuxReadings, MuxAddressChannelPair,
 };
+use hyped_core::config::LOCALISATION_CONFIG;
 use hyped_sensors::SensorValueRange::*;
 use panic_probe as _;
 use static_cell::StaticCell;
+const NUM_ACCELEROMETERS: usize = LOCALISATION_CONFIG.accelerometers.num_sensors as usize;
 
 type I2c1Bus = Mutex<NoopRawMutex, RefCell<I2c<'static, Blocking>>>;
 
 static ACCELERATION_MUX_READINGS: Watch<CriticalSectionRawMutex, AccelerometerMuxReadings, 1> =
     Watch::new();
 
+const MUX_ADDRESS_CHANNEL_PAIRS: Vec<MuxAddressChannelPair, NUM_ACCELEROMETERS> = Vec::new(); // addresses and channels will be determined later
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) -> ! {
     let p = embassy_stm32::init(Default::default());
     let i2c = I2c::new_blocking(p.I2C1, p.PB8, p.PB9, Hertz(200_000), Default::default());
 
-    // Initialize the I2C bus and store it in a static cell so that it can be accessed from the tasks.
+    // Initialize the I2C bus and store it in a static cell so that it can be accessed from the task.
     static I2C_BUS: StaticCell<I2c1Bus> = StaticCell::new();
     let i2c_bus = I2C_BUS.init(Mutex::new(RefCell::new(i2c)));
     defmt::info!("I2C initialized.");
@@ -43,6 +48,7 @@ async fn main(spawner: Spawner) -> ! {
     spawner
         .spawn(read_accelerometer_from_mux(
             i2c_bus,
+            MUX_ADDRESS_CHANNEL_PAIRS,
             accelerometer_mux_reading_sender,
         ))
         .unwrap();
@@ -86,6 +92,6 @@ async fn main(spawner: Spawner) -> ! {
                 }
             }
         }
-        Timer::after(Duration::from_millis(1000)).await;
+        Timer::after(Duration::from_millis(100)).await;
     }
 }
--- boards/stm32f767zi/src/tasks/sensors/read_accelerometer_from_mux.rs
+++ boards/stm32f767zi/src/tasks/sensors/read_accelerometer_from_mux.rs
@@ -11,48 +11,48 @@ use embassy_sync::{
 };
 use embassy_time::{Duration, Timer};
 use heapless::Vec;
+use hyped_core::config::LOCALISATION_CONFIG;
 use hyped_i2c::i2c_mux::I2cMux;
 use hyped_sensors::{
     accelerometer::{self, AccelerationValues, Accelerometer, AccelerometerAddresses},
     SensorValueRange::{self},
 };
+const NUM_ACCELEROMETERS: usize = LOCALISATION_CONFIG.accelerometers.num_sensors as usize;
 
-const MUX_ADDRESS: u8 = 0x70;
-const NUM_ACCELEROMETERS: usize = 4;
+pub struct MuxAddressChannelPair {
+    pub address: u8,
+    pub channel: u8,
+}
 
 pub type AccelerometerMuxReadings =
     Vec<Option<SensorValueRange<AccelerationValues>>, NUM_ACCELEROMETERS>;
 
 type I2c1Bus = Mutex<NoopRawMutex, RefCell<I2c<'static, Blocking>>>;
 
-/// Task that reads the accelerometer from a specific channel of an I2C Mux.
+/// Task that reads the accelerometers on the muxes given in `mux_address_channel_pairs`
 #[embassy_executor::task]
 pub async fn read_accelerometer_from_mux(
     i2c_bus: &'static I2c1Bus,
+    mut mux_address_channel_pairs: Vec<MuxAddressChannelPair, NUM_ACCELEROMETERS>,
     sender: Sender<'static, CriticalSectionRawMutex, AccelerometerMuxReadings, 1>,
 ) -> ! {
-    let mut i2c_muxes: Vec<I2cMux<Stm32f767ziI2c<'_>>, NUM_ACCELEROMETERS> = Vec::new();
+    let mut i2c_muxes: Vec<I2cMux<Stm32f767ziI2c<'_>>, NUM_ACCELEROMETERS> =
+        mux_address_channel_pairs
+            .iter_mut()
+            .map(|pair| {
+                let hyped_i2c = Stm32f767ziI2c::new(i2c_bus);
+                let i2c_mux = match I2cMux::new(hyped_i2c, pair.channel, pair.address) {
+                    Ok(i2c_mux) => i2c_mux,
+                    Err(_) => panic!(
+                        "Failed to create I2C Mux on address {} and channel {}.
+                     Check the wiring and address of the Mux",
+                        pair.address, pair.channel
+                    ),
+                };
 
-    for i in 0..NUM_ACCELEROMETERS as u8 {
-        let channel = i;
-
-        // First, we create a HypedI2c object that wraps the I2C bus.
-        let hyped_i2c = Stm32f767ziI2c::new(i2c_bus);
-
-        // For each one of the four accelerometers, we create a mux object that wraps the HypedI2c object. `i2c_mux` can now be used anywhere that
-        // `hyped_i2c` could be used, but it will automatically switch to the correct channel before sending any I2C commands.
-        let i2c_mux = match I2cMux::new(hyped_i2c, channel, MUX_ADDRESS) {
-            Ok(i2c_mux) => i2c_mux,
-            Err(_) => {
-                panic!("Failed to create I2C Mux. Check the wiring and the I2C address of the Mux.")
-            }
-        };
-
-        match i2c_muxes.push(i2c_mux) {
-            Ok(_) => {}
-            Err(_) => panic!("Failed to add I2C mux to mux vector."),
-        }
-    }
+                i2c_mux
+            })
+            .collect();
 
     // Create a vector of accelerometers using the vector of muxes.
     let mut accelerometers : Vec<Option<Accelerometer<'_, I2cMux<Stm32f767ziI2c<'_>>>>, NUM_ACCELEROMETERS> =
