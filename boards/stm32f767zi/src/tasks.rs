720686949f6aab3e3dc376ce6fcf649812257fa5
SinY/CosY <elitan.uk@gmail.com>
2025-06-06 16:24:43 +0800

    SNS - High Pressure Sensor (#68)
    Co-authored-by: H-Allen <harveyjallen1@gmail.com>
    Co-authored-by: H-Allen <100712070+H-Allen@users.noreply.github.com>

--- boards/stm32f767zi/src/tasks.rs
+++ boards/stm32f767zi/src/tasks.rs
@@ -2,5 +2,6 @@ pub mod can;
 pub mod can_to_mqtt;
 pub mod mqtt;
 pub mod network;
+pub mod read_high_pressure;
 pub mod sensors;
 pub mod state_machine;
--- lib/sensors/src/lib.rs
+++ lib/sensors/src/lib.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(test), no_std)]
 
+pub mod high_pressure;
 pub mod keyence;
 pub mod laser_triangulation;
 pub mod low_pressure;
