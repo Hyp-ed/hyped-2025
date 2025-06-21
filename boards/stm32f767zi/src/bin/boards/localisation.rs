ce9532ce784bf24ef098c80a89b6e2b0c06692c1
David Beechey <david@dtbeechey.dev>
2025-06-21 10:44:23 +0100

    start working on finishing localisation

--- boards/stm32f767zi/src/bin/boards/localisation.rs
+++ boards/stm32f767zi/src/bin/boards/localisation.rs
@@ -14,19 +14,25 @@ use embassy_time::{Duration, Timer};
 use heapless::Vec;
 use hyped_boards_stm32f767zi::{
     io::{Stm32f767ziGpioOutput, Stm32f767ziSpi},
-    tasks::read_keyence::read_keyence,
+    tasks::sensors::{read_keyence::read_keyence, read_optical_flow::read_optical_flow},
 };
+use hyped_core::config::{MeasurementId, LOCALISATION_CONFIG};
 use hyped_localisation::{
-    control::localizer::Localizer,
-    types::{RawAccelerometerData, NUM_ACCELEROMETERS, NUM_AXIS},
+    control::localizer::Localizer, preprocessing::optical, types::RawAccelerometerData,
 };
-use hyped_sensors::optical_flow::OpticalFlow;
 use hyped_spi::HypedSpiCsPin;
 use panic_probe as _;
 
-/// A Watch to hold the latest Keyence stripe count.
+/// A Watch to hold the latest Keyence stripe count
 static KEYENCE_1_STRIPE_COUNT: Watch<CriticalSectionRawMutex, u32, 1> = Watch::new();
 static KEYENCE_2_STRIPE_COUNT: Watch<CriticalSectionRawMutex, u32, 1> = Watch::new();
+
+/// A Watch to hold the latest optical flow data
+static OPTICAL_FLOW_DATA: Watch<
+    CriticalSectionRawMutex,
+    Vec<f64, { LOCALISATION_CONFIG.optical_flow.num_sensors as usize }>,
+    1,
+> = Watch::new();
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) -> ! {
@@ -38,7 +44,7 @@ async fn main(spawner: Spawner) -> ! {
     spi_config.bit_order = BitOrder::MsbFirst;
 
     let spi = Spi::new_blocking(p.SPI1, p.PB3, p.PB5, p.PB4, spi_config);
-    let mut hyped_spi = Stm32f767ziSpi::new(spi);
+    let hyped_spi = Stm32f767ziSpi::new(spi);
 
     let cs = HypedSpiCsPin::new(Stm32f767ziGpioOutput::new(Output::new(
         p.PA4,
@@ -46,35 +52,39 @@ async fn main(spawner: Spawner) -> ! {
         Speed::VeryHigh,
     )));
 
-    let mut optical_flow = OpticalFlow::new(&mut hyped_spi, cs)
-        .await
-        .expect("Failed to initialize optical flow sensor.");
+    spawner
+        .spawn(read_optical_flow(hyped_spi, cs, OPTICAL_FLOW_DATA.sender()))
+        .unwrap();
 
-    defmt::info!("Optical flow sensor initialized.");
+    spawner
+        .spawn(read_keyence(
+            Input::new(p.PC13, Pull::Down),
+            MeasurementId::Keyence1,
+            KEYENCE_1_STRIPE_COUNT.sender(),
+        ))
+        .unwrap();
+    spawner
+        .spawn(read_keyence(
+            Input::new(p.PC14, Pull::Down),
+            MeasurementId::Keyence2,
+            KEYENCE_2_STRIPE_COUNT.sender(),
+        ))
+        .unwrap();
 
-    let gpio_pin1 = Input::new(p.PC13, Pull::Down);
-    let gpio_pin2 = Input::new(p.PC14, Pull::Down);
-
-    // Create a sender and a receiver for the Keyence stripe count.
-    let sender1 = KEYENCE_1_STRIPE_COUNT.sender();
-    let mut receiver1 = KEYENCE_1_STRIPE_COUNT.receiver().unwrap();
-    let sender2 = KEYENCE_2_STRIPE_COUNT.sender();
-    let mut receiver2 = KEYENCE_2_STRIPE_COUNT.receiver().unwrap();
-
-    spawner.spawn(read_keyence(gpio_pin1, sender1)).unwrap();
-    spawner.spawn(read_keyence(gpio_pin2, sender2)).unwrap();
-
-    info!("Starting localizer loop...");
+    // Initialise receivers
+    let mut keyence_1_receiver = KEYENCE_1_STRIPE_COUNT.receiver().unwrap();
+    let mut keyence_2_receiver = KEYENCE_2_STRIPE_COUNT.receiver().unwrap();
+    let mut optical_flow_receiver = OPTICAL_FLOW_DATA.receiver().unwrap();
+    let mut accelerometers_receiver = 
 
     let mut localizer = Localizer::new();
 
+    info!("Starting localizer loop...");
+
     loop {
         // Wait for new Keyence stripe count.
-        let stripe_count1 = receiver1.get().await;
-        let stripe_count2 = receiver2.get().await;
-
-        let flow = optical_flow.get_motion().await.unwrap();
-        let optical_data: Vec<f64, 2> = Vec::from_slice(&[flow.x as f64, flow.y as f64]).unwrap();
+        let stripe_count1 = keyence_1_receiver.get().await;
+        let stripe_count2 = keyence_2_receiver.get().await;
 
         defmt::info!(
             "New Keyence stripe counts: sensor1 = {}, sensor2 = {}",
@@ -84,14 +94,18 @@ async fn main(spawner: Spawner) -> ! {
 
         // Create the sensor data. (no accelerometer data)
         let keyence_data: Vec<u32, 2> = Vec::from_slice(&[stripe_count1, stripe_count2]).unwrap();
-        let accelerometer_data: RawAccelerometerData<NUM_ACCELEROMETERS, NUM_AXIS> =
-            RawAccelerometerData::from_slice(&[
-                Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
-                Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
-                Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
-                Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
-            ])
-            .unwrap();
+        let accelerometer_data: RawAccelerometerData<
+            { LOCALISATION_CONFIG.accelerometers.num_sensors as usize },
+            { LOCALISATION_CONFIG.num_axis as usize },
+        > = RawAccelerometerData::from_slice(&[
+            Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
+            Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
+            Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
+            Vec::from_slice(&[0.0, 0.0, 9.81]).unwrap(),
+        ])
+        .unwrap();
+
+        let optical_data = optical_flow_receiver.get().await;
 
         match localizer.iteration(optical_data, keyence_data, accelerometer_data) {
             Ok(()) => {
--- boards/stm32f767zi/src/tasks/sensors.rs
+++ boards/stm32f767zi/src/tasks/sensors.rs
@@ -3,4 +3,5 @@ pub mod read_accelerometers_from_mux;
 pub mod read_keyence;
 pub mod read_laser_triangulation;
 pub mod read_low_pressure;
+pub mod read_optical_flow;
 pub mod read_temperature;
